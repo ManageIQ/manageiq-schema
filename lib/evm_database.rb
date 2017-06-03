1667224a4f2023efb10d1ea0f3d34317aad33ea8
Jason Frey <fryguy9@gmail.com>
2017-06-03 00:01:28 -0400

    Remove ManageIQ specific code.

--- lib/evm_database.rb
+++ lib/evm_database.rb
@@ -1,112 +1,5 @@
 class EvmDatabase
-  include Vmdb::Logging
-
-  SCHEMA_FILE = Rails.root.join("db/schema.yml").freeze
-
-  PRIMORDIAL_CLASSES = %w(
-    MiqDatabase
-    MiqRegion
-    MiqEnterprise
-    Zone
-    MiqServer
-    ServerRole
-    Tenant
-    MiqProductFeature
-    MiqUserRole
-    MiqGroup
-    User
-    MiqReport
-    VmdbDatabase
-  )
-
-  ORDERED_CLASSES = %w(
-    RssFeed
-    MiqWidget
-    MiqAction
-    MiqEventDefinitionSet
-    MiqEventDefinition
-    MiqPolicySet
-    ChargebackRateDetailMeasure
-    ChargeableField
-    ChargebackRateDetailCurrency
-    ChargebackRate
-  ).freeze
-
-  RAILS_ENGINE_MODEL_CLASS_NAMES = %w(MiqAeDatastore)
-
-  def self.find_seedable_model_class_names
-    @found_model_class_names ||= begin
-      Dir.glob(Rails.root.join("app/models/*.rb")).collect { |f| File.basename(f, ".*").camelize if File.read(f).include?("self.seed") }.compact.sort
-    end
-  end
-
-  def self.seedable_model_class_names
-    ORDERED_CLASSES + (find_seedable_model_class_names - ORDERED_CLASSES) + RAILS_ENGINE_MODEL_CLASS_NAMES
-  end
-
-  def self.seed_primordial
-    if ENV['SKIP_SEEDING'] && MiqDatabase.count > 0
-      puts "** seedings is skipped on startup."
-      puts "** Unset SKIP_SEEDING to re-enable"
-    else
-      seed(PRIMORDIAL_CLASSES)
-    end
-  end
-
-  def self.seed_last
-    unless ENV['SKIP_SEEDING'] && MiqDatabase.count > 0
-      seed(seedable_model_class_names - PRIMORDIAL_CLASSES)
-    end
-  end
-
-  def self.seed(classes = nil, exclude_list = [])
-    _log.info("Seeding...")
-
-    classes ||= PRIMORDIAL_CLASSES + (seedable_model_class_names - PRIMORDIAL_CLASSES)
-    classes -= exclude_list
-
-    # Only 1 machine can go through this at a time
-    # Populating the DB takes 20 seconds
-    # Not populating the db takes 3 seconds
-    MiqDatabase.with_lock(10.minutes) do
-      classes.each do |klass|
-        begin
-          klass = klass.constantize if klass.kind_of?(String)
-        rescue => err
-          _log.log_backtrace(err)
-          raise
-        end
-
-        if klass.respond_to?(:seed)
-          _log.info("Seeding #{klass}")
-          begin
-            klass.seed
-          rescue => err
-            _log.log_backtrace(err)
-            raise
-          end
-        else
-          _log.error("Class #{klass} does not have a seed")
-        end
-      end
-    end
-
-    _log.info("Seeding... Complete")
-  end
-
-  def self.host
-    ActiveRecord::Base.configurations.fetch_path(ENV['RAILS_ENV'], 'host')
-  end
-
-  def self.local?
-    host.blank? || ["localhost", "localhost.localdomain", "127.0.0.1", "0.0.0.0"].include?(host)
-  end
-
-  # Determines the average time to the database in milliseconds
-  def self.ping(connection = ActiveRecord::Base.connection)
-    query = "SELECT 1"
-    Benchmark.realtime { 10.times { connection.select_value(query) } } / 10 * 1000
-  end
+  SCHEMA_FILE = ManageIQ::Schema::Engine.root.join("db/schema.yml").freeze
 
   # Determines if the schema currently being used is the same as the one we expect
   #
@@ -121,11 +14,6 @@ class EvmDatabase
   # @param connection Write the schema at this connection to the file
   def self.write_expected_schema(connection = ActiveRecord::Base.connection)
     File.write(SCHEMA_FILE, current_schema(connection).to_yaml)
-  end
-
-  def self.raise_server_event(event)
-    msg = "Server IP: #{MiqServer.my_server.ipaddress}, Server Host Name: #{MiqServer.my_server.hostname}"
-    MiqEvent.raise_evm_event_queue(MiqServer.my_server, event, :event_details => msg)
   end
 
   class << self
--- spec/lib/evm_database_spec.rb
+++ spec/lib/evm_database_spec.rb
@@ -1,34 +1,5 @@
 describe EvmDatabase do
   subject { described_class }
-  context "#local?" do
-    ["localhost", "127.0.0.1", "", nil].each do |host|
-      it "should know #{host} is local" do
-        expect(subject).to receive(:host).at_least(:once).and_return(host)
-        expect(subject).to be_local
-      end
-    end
-
-    it "should know otherhost is not local" do
-      expect(subject).to receive(:host).twice.and_return("otherhost")
-      expect(subject).not_to be_local
-    end
-  end
-
-  context "#seed_primordial" do
-    it "populates seeds" do
-      described_class::PRIMORDIAL_CLASSES.each { |klass| expect(klass.constantize).to receive(:seed) }
-      described_class.seed_primordial
-    end
-  end
-
-  describe ".find_seedable_model_class_names" do
-    it "returns ordered classes first" do
-      stub_const("EvmDatabase::ORDERED_CLASSES", %w(a z))
-      stub_const("EvmDatabase::RAILS_ENGINE_MODEL_CLASS_NAMES", [])
-      expect(described_class).to receive(:find_seedable_model_class_names).and_return(%w(a c z))
-      expect(described_class.seedable_model_class_names).to eq(%w(a z c))
-    end
-  end
 
   context "schema checking" do
     def stub_test_database(db_hash)
@@ -167,17 +138,6 @@ describe EvmDatabase do
 
           expect(subject.check_schema(conn)).to match(/Schema validation failed for host 192\.168\.1\.2:.*/)
         end
-      end
-    end
-
-    describe ".raise_server_event" do
-      it "adds to queue request to raise 'evm_event'" do
-        EvmSpecHelper.create_guid_miq_server_zone
-        described_class.raise_server_event("db_failover_executed")
-        record = MiqQueue.last
-        expect(record.class_name). to eq "MiqEvent"
-        expect(record.method_name).to eq "raise_evm_event"
-        expect(record.args[1]).to eq "db_failover_executed"
       end
     end
   end
