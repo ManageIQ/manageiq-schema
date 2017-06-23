0d1ff8aa1072b18d2a1c250269b0a7977d8b3b7d
skovic <skovic@lenovo.com>
2017-06-23 14:34:25 -0400

    Remove index names

--- db/migrate/20170530180114_add_manufacturer_and_fru_to_guest_devices.rb
+++ db/migrate/20170530180114_add_manufacturer_and_fru_to_guest_devices.rb
@@ -3,6 +3,6 @@ class AddManufacturerAndFruToGuestDevices < ActiveRecord::Migration[5.0]
     add_column :guest_devices, :manufacturer, :string
     add_column :guest_devices, :field_replaceable_unit, :string
     add_column :guest_devices, :parent_device_id, :bigint
-    add_index :guest_devices, :parent_device_id, :name => "index_guest_devices_on_parent_device_id"
+    add_index :guest_devices, :parent_device_id
   end
 end
