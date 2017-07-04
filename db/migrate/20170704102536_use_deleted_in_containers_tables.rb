249f7ec083cea117013afd150a327db73c721bd9
Ladislav Smola <lsmola@redhat.com>
2017-07-04 09:30:50 +0200

    Use soft delete instead of disconnect
    Use soft delete instead of disconnect

---
+++ db/migrate/20170704102536_use_deleted_in_containers_tables.rb
@@ -0,0 +1,55 @@
+class UseDeletedInContainersTables < ActiveRecord::Migration[5.0]
+  class ContainerDefinition < ActiveRecord::Base;
+  end
+
+  class ContainerGroup < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+  end
+
+  class ContainerImage < ActiveRecord::Base;
+  end
+
+  class ContainerProject < ActiveRecord::Base;
+  end
+
+  class ContainerNode < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+  end
+
+  class Container < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+  end
+
+  def disconnect_to_soft_delete(model)
+    model.where.not(:deleted_on => nil).update_all("ems_id = old_ems_id")
+  end
+
+  def soft_delete_to_disconnect(model)
+    model.where.not(:deleted_on => nil).update_all(:ems_id => nil)
+  end
+
+  MODEL_CLASSES = [
+    ContainerDefinition,
+    ContainerGroup,
+    ContainerImage,
+    ContainerProject,
+    ContainerNode,
+    Container,
+  ].freeze
+
+  def up
+    MODEL_CLASSES.each do |model_class|
+      say_with_time("Change ':deleted_on not nil' :ems_id to :old_ems_id for #{model_class}") do
+        disconnect_to_soft_delete(model_class)
+      end
+    end
+  end
+
+  def down
+    MODEL_CLASSES.each do |model_class|
+      say_with_time("Change ':deleted_on not nil' :ems_id to nil for #{model_class}") do
+        soft_delete_to_disconnect(model_class)
+      end
+    end
+  end
+end
