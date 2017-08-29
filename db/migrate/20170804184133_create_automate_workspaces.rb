b3608e9dcef08295c7dfac71cfed45917f1e6350
mkanoor <mkanoor@redhat.com>
2017-08-29 15:29:40 -0400

    Added user_id and tenant_id so we can do RBAC

--- db/migrate/20170804184133_create_automate_workspaces.rb
+++ db/migrate/20170804184133_create_automate_workspaces.rb
@@ -2,6 +2,8 @@ class CreateAutomateWorkspaces < ActiveRecord::Migration[5.0]
   def change
     create_table :automate_workspaces do |t|
       t.string :guid
+      t.bigint :user_id
+      t.bigint :tenant_id
       t.jsonb :output
       t.jsonb :input
     end
