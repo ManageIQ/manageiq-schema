75deb24633a6eb891402925a856da449798f4ee1
skovic <skovic@lenovo.com>
2017-09-07 12:19:11 -0400

    Rename external_id to ems_ref

--- db/migrate/20170905210626_create_configuration_templates.rb
+++ db/migrate/20170905210626_create_configuration_templates.rb
@@ -2,7 +2,7 @@ class CreateConfigurationTemplates < ActiveRecord::Migration[5.0]
   def change
     create_table :configuration_templates do |t|
       t.bigint :ems_id
-      t.string :external_id
+      t.string :ems_ref
       t.string :name
       t.string :description
       t.boolean :user_defined
