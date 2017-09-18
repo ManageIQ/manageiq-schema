ca38121d70328d1fef562f74f9f76e7ffe554734
Alberto Jesus Gutierrez Juanes <alberto@iMac-de-Alberto.local>
2017-09-18 21:14:28 +0200

    Fix issue with showback_rate belongs and type of the data in steps

---
+++ db/migrate/20170918190738_issue_shorback_tiers.rb
@@ -0,0 +1,13 @@
+class IssueShorbackTiers < ActiveRecord::Migration[5.0]
+  def up
+    rename_column    :showback_tiers, :showback_rates_id, :showback_rate_id
+    change_column    :showback_tiers, :tier_start_value,  :float
+    change_column    :showback_tiers, :tier_end_value,    :float
+  end
+
+  def down
+    rename_column    :showback_tiers, :showback_rate_id,   :showback_rates_id
+    change_column    :showback_tiers, :tier_start_value, :bigint
+    change_column    :showback_tiers, :tier_end_value,   :bigint
+  end
+end
