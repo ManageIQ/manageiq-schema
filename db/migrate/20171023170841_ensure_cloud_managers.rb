57f48ab3fd9147666a6d65d4844bd915751ab206
Daniel Berger <dberger@redhat.com>
2017-10-27 10:16:28 -0600

    Add local definition of ensure_managers and switch to 'up' from 'change' since it's non-reversible.

--- db/migrate/20171023170841_ensure_cloud_managers.rb
+++ db/migrate/20171023170841_ensure_cloud_managers.rb
@@ -1,5 +1,39 @@
+class ManageIQ::Providers::CloudManager
+  def ensure_managers
+    build_network_manager unless network_manager
+    network_manager.name = "#{name} Network Manager"
+
+    build_ebs_storage_manager unless ebs_storage_manager
+    ebs_storage_manager.name = "#{name} EBS Storage Manager"
+
+    if ::Settings.prototype.amazon.s3
+      build_s3_storage_manager unless s3_storage_manager
+      s3_storage_manager.name = "#{name} S3 Storage Manager"
+    end
+
+    ensure_managers_zone_and_provider_region
+  end
+
+  def ensure_managers_zone_and_provider_region
+    if network_manager
+      network_manager.zone_id         = zone_id
+      network_manager.provider_region = provider_region
+    end
+
+    if ebs_storage_manager
+      ebs_storage_manager.zone_id         = zone_id
+      ebs_storage_manager.provider_region = provider_region
+    end
+
+    if s3_storage_manager
+      s3_storage_manager.zone_id         = zone_id
+      s3_storage_manager.provider_region = provider_region
+    end
+  end
+end
+
 class EnsureCloudManagers < ActiveRecord::Migration[5.0]
-  def change
+  def up
     ManageIQ::Providers::CloudManager.all.each { |x| x.send(:ensure_managers); x.save!; }
   end
 end
