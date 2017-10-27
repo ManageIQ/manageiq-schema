ff9a724270038a33c3718c7fb480cc966cc17e2f
Daniel Berger <dberger@redhat.com>
2017-10-27 12:02:50 -0600

    Attempt to scope CloudManager.

--- db/migrate/20171023170841_ensure_cloud_managers.rb
+++ db/migrate/20171023170841_ensure_cloud_managers.rb
@@ -3,41 +3,45 @@ class EnsureCloudManagers < ActiveRecord::Migration[5.0]
   class BaseManager < ExtManagementSystem; end
   class Settings < ActiveRecord::Base; end
 
-  class CloudManager < ActiveRecord::Base
-    def ensure_managers
-      build_network_manager unless network_manager
-      network_manager.name = "#{name} Network Manager"
+  class ManageIQ
+    class Providers
+      class CloudManager < ActiveRecord::Base
+        def ensure_managers
+          build_network_manager unless network_manager
+          network_manager.name = "#{name} Network Manager"
 
-      build_ebs_storage_manager unless ebs_storage_manager
-      ebs_storage_manager.name = "#{name} EBS Storage Manager"
+          build_ebs_storage_manager unless ebs_storage_manager
+          ebs_storage_manager.name = "#{name} EBS Storage Manager"
 
-      if Settings.prototype.amazon.s3
-        build_s3_storage_manager unless s3_storage_manager
-        s3_storage_manager.name = "#{name} S3 Storage Manager"
-      end
+          if Settings.prototype.amazon.s3
+            build_s3_storage_manager unless s3_storage_manager
+            s3_storage_manager.name = "#{name} S3 Storage Manager"
+          end
 
-      ensure_managers_zone_and_provider_region
-    end
+          ensure_managers_zone_and_provider_region
+        end
 
-    def ensure_managers_zone_and_provider_region
-      if network_manager
-        network_manager.zone_id         = zone_id
-        network_manager.provider_region = provider_region
-      end
+        def ensure_managers_zone_and_provider_region
+          if network_manager
+            network_manager.zone_id         = zone_id
+            network_manager.provider_region = provider_region
+          end
 
-      if ebs_storage_manager
-        ebs_storage_manager.zone_id         = zone_id
-        ebs_storage_manager.provider_region = provider_region
-      end
+          if ebs_storage_manager
+            ebs_storage_manager.zone_id         = zone_id
+            ebs_storage_manager.provider_region = provider_region
+          end
 
-      if s3_storage_manager
-        s3_storage_manager.zone_id         = zone_id
-        s3_storage_manager.provider_region = provider_region
+          if s3_storage_manager
+            s3_storage_manager.zone_id         = zone_id
+            s3_storage_manager.provider_region = provider_region
+          end
+        end
       end
     end
   end
 
   def up
-    CloudManager.all.each { |x| x.send(:ensure_managers); x.save!; }
+    ManageIQ::Providers::CloudManager.all.each { |x| x.send(:ensure_managers); x.save!; }
   end
 end
