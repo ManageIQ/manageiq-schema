23af564f190d828eba253e33ca9038dc88e35cb9
Beni Cherniavsky-Paskin <cben@redhat.com>
2017-10-29 11:18:21 +0200

    Test for broken migration as merged
    20171026114327_add_deleted_on_to_container_quota_and_items
    used add_timestamps on existing container_quota_items table.
    For poeple with any records in container_quota_items, it failed with:
    
      PG::NotNullViolation: ERROR:  column "created_at" contains null values
      : ALTER TABLE "container_quota_items" ADD "created_at" timestamp NOT NULL
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1504560
    fbf0077

--- db/migrate/20171026114327_add_deleted_on_to_container_quota_and_items.rb
+++ db/migrate/20171026114327_add_deleted_on_to_container_quota_and_items.rb
@@ -1,4 +1,7 @@
 class AddDeletedOnToContainerQuotaAndItems < ActiveRecord::Migration[5.0]
+  class ContainerQuota < ActiveRecord::Base; end
+  class ContainerQuotaItem < ActiveRecord::Base; end
+
   def change
     add_column :container_quotas, :deleted_on, :datetime
     add_index :container_quotas, :deleted_on
