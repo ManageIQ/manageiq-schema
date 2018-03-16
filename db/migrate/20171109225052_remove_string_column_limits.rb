570d267c5bf6d73b90766c947c57aece667c0403
Nick Carboni <ncarboni@redhat.com>
2018-03-16 11:54:02 -0400

    Remove the limits from the tables in sorted order
    This will prevent us from trying to alter an inherited column
    in a table like metrics_01 or similar

--- db/migrate/20171109225052_remove_string_column_limits.rb
+++ db/migrate/20171109225052_remove_string_column_limits.rb
@@ -1,6 +1,6 @@
 class RemoveStringColumnLimits < ActiveRecord::Migration[5.0]
   def up
-    connection.tables.each do |t|
+    connection.tables.sort.each do |t|
       connection.columns(t).each do |col|
         next unless col.type == :string && !col.limit.nil?
         change_column t, col.name, :string, :limit => nil
