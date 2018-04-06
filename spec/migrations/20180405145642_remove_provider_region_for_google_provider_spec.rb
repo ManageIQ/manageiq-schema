6ec0ed852c9ce521e652f3154f7586417af42d03
Zita Nemeckova <znemecko@redhat.com>
2018-04-06 16:20:50 +0200

    Fix specs according to review

--- spec/migrations/20180405145642_remove_provider_region_for_google_provider_spec.rb
+++ spec/migrations/20180405145642_remove_provider_region_for_google_provider_spec.rb
@@ -1,10 +1,10 @@
 require_migration
 
 describe RemoveProviderRegionForGoogleProvider do
-  let(:ems_stub) { migration_stub :ExtManagementSystem }
-
   migration_context :up do
-    before do
+    let(:ems_stub) { migration_stub :ExtManagementSystem }
+
+    it 'does sets provider_region to nil only to Google provider' do
       ems_stub.create!(:type            => 'ManageIQ::Providers::Google::CloudManager',
                        :provider_region => 42)
       ems_stub.create!(:type            => 'ManageIQ::Providers::Amazon::CloudManager',
@@ -17,13 +17,8 @@ describe RemoveProviderRegionForGoogleProvider do
                        :provider_region => 42)
 
       migrate
-    end
 
-    it 'sets provider_region to nil for ManageIQ::Providers::Google::CloudManager' do
       expect(ems_stub.where(:provider_region => nil).count).to eq 1
-    end
-
-    it 'does not set provider_region to nil for other providers' do
       expect(ems_stub.where(:provider_region => 42).count).to eq 4
     end
   end
