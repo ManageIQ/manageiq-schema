7eb7aeb9ead96556d9388135341ae08803c4bacc
Zita Nemeckova <znemecko@redhat.com>
2018-04-06 09:49:28 +0200

    Add check provider_region is not set to nil for non-Google provider

--- spec/migrations/20180405145642_remove_provider_region_for_google_provider_spec.rb
+++ spec/migrations/20180405145642_remove_provider_region_for_google_provider_spec.rb
@@ -9,12 +9,22 @@ describe RemoveProviderRegionForGoogleProvider do
                        :provider_region => 42)
       ems_stub.create!(:type            => 'ManageIQ::Providers::Amazon::CloudManager',
                        :provider_region => 42)
+      ems_stub.create!(:type            => 'ManageIQ::Providers::Azure::CloudManager',
+                       :provider_region => 42)
+      ems_stub.create!(:type            => 'ManageIQ::Providers::Openstack::CloudManager',
+                       :provider_region => 42)
+      ems_stub.create!(:type            => 'ManageIQ::Providers::Vmware::CloudManager',
+                       :provider_region => 42)
 
       migrate
     end
 
     it 'sets provider_region to nil for ManageIQ::Providers::Google::CloudManager' do
       expect(ems_stub.where(:provider_region => nil).count).to eq 1
+    end
+
+    it 'does not set provider_region to nil for other providers' do
+      expect(ems_stub.where(:provider_region => 42).count).to eq 4
     end
   end
 end
