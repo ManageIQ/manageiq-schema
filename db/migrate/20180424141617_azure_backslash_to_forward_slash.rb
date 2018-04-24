ffc68921dac90107ff21ca00418060eb437d4a17
Daniel Berger <dberger@redhat.com>
2018-04-24 09:52:54 -0600

    Convert backslashes to forward slashes.
    Initial commit.
    
    Optimize stack output and stack parameter.
    
    Add paranoid check for description, optimize event stream update.
    
    Added say_with_time wrappers.
    
    Move vm and template objects into local test block.
    
    Added event stream test.
    
    Move event stream stub creation into spec itself.
    
    Move most of the orchestration stubs into their own specs locally.

---
+++ db/migrate/20180424141617_azure_backslash_to_forward_slash.rb
@@ -0,0 +1,73 @@
+class AzureBackslashToForwardSlash < ActiveRecord::Migration[5.0]
+  class Vm < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+  end
+
+  class OrchestrationStack < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+  end
+
+  class OrchestrationStackOutput < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+  end
+
+  class OrchestrationStackParameter < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+  end
+
+  class OrchestrationStackResource < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+  end
+
+  class EventStream < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+  end
+
+  def up
+    say_with_time("Updating Azure VM ems_ref, uid_ems and description") do
+      Vm.where(:type => "ManageIQ::Providers::Azure::CloudManager::Vm")
+        .update_all("
+          ems_ref = replace(ems_ref, '\\', '/'),
+          uid_ems = replace(uid_ems, '\\', '/'),
+          description = replace(description, '\\', '/')
+        ")
+    end
+
+    say_with_time("Updating Azure template ems_ref, uid_ems and description") do
+      Vm.where(:type => "ManageIQ::Providers::Azure::CloudManager::Template")
+        .update_all("
+          ems_ref = replace(ems_ref, '\\', '/'),
+          uid_ems = replace(uid_ems, '\\', '/'),
+          description = replace(description, '\\', '/')
+        ")
+    end
+
+    say_with_time("Updating Azure orchestration stack ems_ref") do
+      OrchestrationStack
+        .where(:type => "ManageIQ::Providers::Azure::CloudManager::OrchestrationStack")
+        .update_all("ems_ref = replace(ems_ref, '\\', '/'), description = replace(description, '\\', '/')")
+    end
+
+    say_with_time("Updating Azure orchestration stack output ems_ref") do
+      OrchestrationStackOutput
+        .where(:stack_id => OrchestrationStack.where(:type => "ManageIQ::Providers::Azure::CloudManager::OrchestrationStack"))
+        .update_all("ems_ref = replace(ems_ref, '\\', '/')")
+    end
+
+    say_with_time("Updating Azure orchestration stack parameter ems_ref") do
+      OrchestrationStackParameter
+        .where(:stack_id => OrchestrationStack.where(:type => "ManageIQ::Providers::Azure::CloudManager::OrchestrationStack"))
+        .update_all("ems_ref = replace(ems_ref, '\\', '/')")
+    end
+
+    say_with_time("Updating Azure orchestration stack resource ems_ref") do
+      OrchestrationStackResource
+        .where(:stack_id => OrchestrationStack.where(:type => "ManageIQ::Providers::Azure::CloudManager::OrchestrationStack"))
+        .update_all("ems_ref = replace(ems_ref, '\\', '/'), description = replace(description, '\\', '/')")
+    end
+
+    say_with_time("Updating Azure event stream ems_ref") do
+      EventStream.where(:source => "AZURE").update_all("vm_ems_ref = replace(vm_ems_ref, '\\', '/')")
+    end
+  end
+end
