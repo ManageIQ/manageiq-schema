9f04b23987efab53230c617d9a77855ad70b671d
Douglas Gabriel <douglasxgabriel@gmail.com>
2018-05-24 15:23:03 -0300

    Creating physical_components table

---
+++ db/migrate/20180524135736_create_physical_components.rb
@@ -0,0 +1,10 @@
+class CreatePhysicalComponents < ActiveRecord::Migration[5.0]
+  def change
+    create_table :physical_components do |t|
+      t.belongs_to :component, :type => :bigint, :polymorphic => true
+      t.string     :ems_ref
+      t.timestamps :null => true
+      t.index      %i(component_id component_type)
+    end
+  end
+end
