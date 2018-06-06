b2d10567eebecc2c0d6b6d1de15ac61d8e9c3ce7
Dávid Halász <dhalasz@redhat.com>
2018-06-06 11:27:27 +0200

    Convert user quadicon settings from the old format to the new one

---
+++ db/migrate/20180606083431_convert_quadicon_settings_keys.rb
@@ -0,0 +1,25 @@
+class ConvertQuadiconSettingsKeys < ActiveRecord::Migration[5.0]
+  class User < ActiveRecord::Base
+    serialize :settings, Hash
+  end
+
+  def up
+    say_with_time("Converting :ems quadicon settings keys for users to :ems_infra") do
+      User.all.each do |user|
+        settings = user.settings
+        settings[:quadicons][:ems_infra] = settings[:quadicons].delete(:ems)
+        user.update_attributes(:settings => settings)
+      end
+    end
+  end
+
+  def down
+    say_with_time("Converting :ems_infra quadicon settings keys for users to :ems") do
+      User.all.each do |user|
+        settings = user.settings
+        settings[:quadicons][:ems] = settings[:quadicons].delete(:ems_infra)
+        user.update_attributes(:settings => settings)
+      end
+    end
+  end
+end
