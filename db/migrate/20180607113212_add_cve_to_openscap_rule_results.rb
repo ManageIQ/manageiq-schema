baabbf3820e14a920c93eb35c5d28f16ec10ddf0
Ian Watson <ianwatson@redhat.com>
2018-06-07 11:34:19 +0200

    adding migration for cve data for openscap

---
+++ db/migrate/20180607113212_add_cve_to_openscap_rule_results.rb
@@ -0,0 +1,6 @@
+class AddCVEToOpenScapRuleResults < ActiveRecord::Migration[5.0] 
+  def change 
+    add_column :openscap_rule_results, :title, :string 
+    add_column :openscap_rule_results, :cves, :string 
+  end 
+end
