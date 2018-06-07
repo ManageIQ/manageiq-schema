6097cd9f38f845049fd7e665095c25bac17e2c28
Ian Watson <ianwatson@redhat.com>
2018-06-07 11:46:09 +0200

    renaming

--- db/migrate/20180607113212_add_cve_to_openscap_rule_results.rb
+++ db/migrate/20180607113212_add_cve_to_openscap_rule_results.rb
@@ -1,4 +1,4 @@
-class AddCVEToOpenScapRuleResults < ActiveRecord::Migration[5.0] 
+class AddCveToOpenscapRuleResults < ActiveRecord::Migration[5.0] 
   def change 
     add_column :openscap_rule_results, :title, :string 
     add_column :openscap_rule_results, :cves, :string 
