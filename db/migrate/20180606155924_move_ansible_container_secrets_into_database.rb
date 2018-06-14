542171b2b1ac3c0990b5d61cf162f2931c4654f2
Nick Carboni <ncarboni@redhat.com>
2018-06-14 17:31:02 -0400

    Use only core libs to get the secret values
    Also don't try to remove the other objects during the migration.
    We don't want a migration to be tied to a particular gem version
    especially if we also will want to change that gem version in the
    core app.
    
    We will just have to document any other changes to the project
    that need to be made in the course of the upgrade

--- db/migrate/20180606155924_move_ansible_container_secrets_into_database.rb
+++ db/migrate/20180606155924_move_ansible_container_secrets_into_database.rb
@@ -1,10 +1,7 @@
 class MoveAnsibleContainerSecretsIntoDatabase < ActiveRecord::Migration[5.0]
-  require 'kubeclient'
-
   TOKEN_FILE   = "/run/secrets/kubernetes.io/serviceaccount/token".freeze
   CA_CERT_FILE = "/run/secrets/kubernetes.io/serviceaccount/ca.crt".freeze
   SECRET_NAME  = "ansible-secrets".freeze
-  SERVICE_NAME = "ansible".freeze
 
   class MiqDatabase < ActiveRecord::Base; end
 
@@ -15,9 +12,6 @@ class MoveAnsibleContainerSecretsIntoDatabase < ActiveRecord::Migration[5.0]
   def up
     return unless containerized?
     update_authentications
-    delete_deployment_config(SERVICE_NAME)
-    delete_service(SERVICE_NAME)
-    delete_secret(SECRET_NAME)
   end
 
   private
@@ -27,13 +21,12 @@ class MoveAnsibleContainerSecretsIntoDatabase < ActiveRecord::Migration[5.0]
   end
 
   def update_authentications
-    secret_key, rabbit_password, database_password = get_secret_data
+    secret_key, rabbit_password, admin_password, database_password = get_secret_data
 
-    return unless secret_key.present? && rabbit_password.present?
+    return unless secret_key.present? && rabbit_password.present? && admin_password.present?
 
-    db_id = MiqDatabase.first.id
     db_args = {
-      :resource_id   => db_id,
+      :resource_id   => MiqDatabase.first.id,
       :resource_type => "MiqDatabase"
     }
 
@@ -48,6 +41,12 @@ class MoveAnsibleContainerSecretsIntoDatabase < ActiveRecord::Migration[5.0]
       :userid   => "ansible",
       :type     => "AuthUseridPassword"
     )
+    admin_auth_find_args = db_args.merge(
+      :name     => "Ansible Admin Authentication",
+      :authtype => "ansible_admin_password",
+      :userid   => "admin",
+      :type     => "AuthUseridPassword"
+    )
     database_auth_find_args = db_args.merge(
       :name     => "Ansible Database Authentication",
       :authtype => "ansible_database_password",
@@ -57,16 +56,27 @@ class MoveAnsibleContainerSecretsIntoDatabase < ActiveRecord::Migration[5.0]
 
     update_or_create_authentication!(secret_key_find_args, :auth_key => secret_key)
     update_or_create_authentication!(rabbitmq_auth_find_args, :password => rabbit_password)
+    update_or_create_authentication!(admin_auth_find_args, :password => admin_password)
     update_or_create_authentication!(database_auth_find_args, :password => database_password)
   end
 
   def get_secret_data
-    kube_data = kube_connection.get_secret(SECRET_NAME, my_namespace).data
-    decrypted_data = kube_data.to_h.stringify_keys.transform_values { |v| Base64.decode64(v) }
+    require 'open-uri'
+    require 'json'
 
-    [decrypted_data["secret-key"], decrypted_data["rabbit-password"], ApplicationRecord.configurations[Rails.env]["password"]].map { |v| MiqPassword.encrypt(v) }
-  rescue KubeException => e
-    raise unless e.error_code == 404
+    kube_data = secret_uri.open(request_params) do |f|
+      JSON.parse(f.read)["data"]
+    end
+
+    decoded_data = kube_data.transform_values { |v| Base64.decode64(v) }
+
+    [
+      decoded_data["secret-key"],
+      decoded_data["rabbit-password"],
+      decoded_data["admin-password"],
+      ApplicationRecord.configurations[Rails.env]["password"]
+    ].map { |v| MiqPassword.encrypt(v) }
+  rescue OpenURI::HTTPError
     nil
   end
 
@@ -75,67 +85,20 @@ class MoveAnsibleContainerSecretsIntoDatabase < ActiveRecord::Migration[5.0]
     auth.update_attributes!(update_args)
   end
 
-  def delete_deployment_config(name)
-    rc = kube_connection.get_replication_controllers(
-      :label_selector => "openshift.io/deployment-config.name=#{name}",
-      :namespace      => my_namespace
-    ).first
-
-    oc_connection.patch_deployment_config(name, { :spec => { :replicas => 0 } }, my_namespace)
-    oc_connection.delete_deployment_config(name, my_namespace)
-    delete_replication_controller(rc.metadata.name) if rc
-  rescue KubeException => e
-    raise unless e.error_code == 404
-  end
-
-  def delete_replication_controller(name)
-    kube_connection.delete_replication_controller(name, my_namespace)
-  rescue KubeException => e
-    raise unless e.error_code == 404
-  end
-
-  def delete_service(name)
-    kube_connection.delete_service(name, my_namespace)
-  rescue KubeException => e
-    raise unless e.error_code == 404
-  end
-
-  def delete_secret(name)
-    kube_connection.delete_secret(name, my_namespace)
-  rescue KubeException => e
-    raise unless e.error_code == 404
-  end
-
-  def oc_connection
-    @oc_connection ||= raw_connect(manager_uri("/oapi"))
-  end
-
-  def kube_connection
-    @kube_connection ||= raw_connect(manager_uri("/api"))
-  end
-
-  def raw_connect(uri)
-    ssl_options = {
-      :verify_ssl => OpenSSL::SSL::VERIFY_PEER,
-      :ca_file    => CA_CERT_FILE
+  def request_params
+    {
+      'Accept'         => "application/json",
+      'Authorization'  => "Bearer #{File.read(TOKEN_FILE)}",
+      :ssl_ca_cert     => CA_CERT_FILE,
+      :ssl_verify_mode => OpenSSL::SSL::VERIFY_PEER
     }
-
-    Kubeclient::Client.new(
-      uri,
-      :auth_options => { :bearer_token_file => TOKEN_FILE },
-      :ssl_options  => ssl_options
-    )
   end
 
-  def manager_uri(path)
+  def secret_uri
     URI::HTTPS.build(
       :host => ENV["KUBERNETES_SERVICE_HOST"],
       :port => ENV["KUBERNETES_SERVICE_PORT"],
-      :path => path
+      :path => "/api/v1/namespaces/#{ENV["MY_POD_NAMESPACE"]}/secrets/#{SECRET_NAME}"
     )
-  end
-
-  def my_namespace
-    ENV["MY_POD_NAMESPACE"]
   end
 end
