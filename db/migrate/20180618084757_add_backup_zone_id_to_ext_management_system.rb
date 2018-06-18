c3dda9276b9b1a1c2842c586945a20bde40d146c
Martin Slemr <mslemr@redhat.com>
2018-06-18 17:38:44 +0200

    Changed EMS's backup zone to bigint

--- db/migrate/20180618084757_add_backup_zone_id_to_ext_management_system.rb
+++ db/migrate/20180618084757_add_backup_zone_id_to_ext_management_system.rb
@@ -1,5 +1,5 @@
 class AddBackupZoneIdToExtManagementSystem < ActiveRecord::Migration[5.0]
   def change
-    add_column :ext_management_systems, :backup_zone_id, :integer, :after => :zone_id, :index => true
+    add_reference :ext_management_systems, :backup_zone, :type => :bigint, :index => true, :after => :zone_id
   end
 end
