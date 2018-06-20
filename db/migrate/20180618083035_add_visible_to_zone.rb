d5a8feaf91c20aa2aa0842b4ded9e0153da2c71d
Martin Slemr <mslemr@redhat.com>
2018-06-20 17:00:57 +0200

    Fixes Zone namespace

--- db/migrate/20180618083035_add_visible_to_zone.rb
+++ db/migrate/20180618083035_add_visible_to_zone.rb
@@ -3,8 +3,8 @@ class AddVisibleToZone < ActiveRecord::Migration[5.0]
     add_column :zones, :visible, :boolean, :default => true
     reversible do |dir|
       dir.up do
-        Zone.reset_column_information
-        Zone.seed
+        ::Zone.reset_column_information
+        ::Zone.seed
       end
     end
   end
