01927564807366e0d100420208b092f3fab89064
Martin Slemr <mslemr@redhat.com>
2018-07-10 14:01:34 +0200

    Renaming existing zone
    If previously created by user

--- db/migrate/20180626125654_make_maintenance_zone_record.rb
+++ db/migrate/20180626125654_make_maintenance_zone_record.rb
@@ -1,16 +1,32 @@
 class MakeMaintenanceZoneRecord < ActiveRecord::Migration[5.0]
   class Zone < ActiveRecord::Base
+    validates_presence_of   :name
+    validates_uniqueness_of :name
+
+    MAINTENANCE_ZONE_NAME = "__maintenance__".freeze
   end
 
   def up
     say_with_time("Creating Maintenance Zone") do
-      Zone.create_with(:description => "Maintenance Zone", :visible => false).find_or_create_by!(:name => 'maintenance')
+      zone = Zone.where(:name => Zone::MAINTENANCE_ZONE_NAME).first
+      if zone.present?
+        zone.name = "#{zone.name}_0"
+        zone.save
+      end
+
+      Zone.create_with(:description => "Maintenance Zone", :visible => false).find_or_create_by!(:name => Zone::MAINTENANCE_ZONE_NAME)
     end
   end
 
   def down
     say_with_time("Deleting Maintenance Zone") do
-      Zone.where(:name => 'maintenance').where(:visible => false).destroy_all
+      Zone.where(:name => Zone::MAINTENANCE_ZONE_NAME).where(:visible => false).destroy_all
+
+      orig = Zone.where(:name => "#{Zone::MAINTENANCE_ZONE_NAME}_0").first
+      if orig.present?
+        orig.name = orig.name[0..orig.name.size - 3]
+        orig.save
+      end
     end
   end
 end
--- spec/migrations/20180626125654_make_maintenance_zone_record_spec.rb
+++ spec/migrations/20180626125654_make_maintenance_zone_record_spec.rb
@@ -4,22 +4,48 @@ describe MakeMaintenanceZoneRecord do
   let(:zone_stub) { migration_stub(:Zone) }
 
   migration_context :up do
+    before(:each) do
+      zone_stub.delete_all
+    end
+
     it "adds MaintenanceZone" do
       migrate
 
-      expect(zone_stub.where(:name => 'maintenance').where(:visible => false).count).to eq(1)
+      expect(zone_stub.where(:name => zone_stub::MAINTENANCE_ZONE_NAME).where(:visible => false).count).to eq(1)
+    end
+
+    it "renames original maintenance zone" do
+      orig = zone_stub.create!(:name => zone_stub::MAINTENANCE_ZONE_NAME)
+
+      migrate
+      orig.reload
+
+      expect(orig.name).to eq("#{zone_stub::MAINTENANCE_ZONE_NAME}_0")
     end
   end
 
   migration_context :down do
+    before(:each) do
+      zone_stub.delete_all
+    end
+
     it "removes MaintenanceZone" do
-      zone_stub.create!(:name        => 'maintenance',
+      zone_stub.create!(:name        => zone_stub::MAINTENANCE_ZONE_NAME,
                         :description => 'Maintenance Zone',
                         :visible     => false)
 
       migrate
 
-      expect(zone_stub.where(:name => 'maintenance').where(:visible => false).count).to eq(0)
+      expect(zone_stub.where(:name => zone_stub::MAINTENANCE_ZONE_NAME).where(:visible => false).count).to eq(0)
+    end
+
+    it "renames original maintenance zone back" do
+      orig = zone_stub.create!(:name => "#{zone_stub::MAINTENANCE_ZONE_NAME}_0")
+
+      migrate
+      orig.reload
+
+      expect(orig.name).to eq(zone_stub::MAINTENANCE_ZONE_NAME)
     end
   end
 end
