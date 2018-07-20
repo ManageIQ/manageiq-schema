35765f15d3d512767c9ab2fc6168efd8316e38ee
hsong-rh <hsong@redhat.com>
2018-07-20 15:48:05 -0400

    Changed to use reserve_table to migrate

--- spec/migrations/20180719163110_add_internal_to_service_template_spec.rb
+++ spec/migrations/20180719163110_add_internal_to_service_template_spec.rb
@@ -1,23 +1,37 @@
 require_migration
 
 describe AddInternalToServiceTemplate do
+  let(:reserve_stub)          { Spec::Support::MigrationStubs.reserved_stub }
   let(:service_template_stub) { migration_stub(:ServiceTemplate) }
 
   migration_context :up do
-    it "sets internal to true when type is ServiceTemplateTransformationPlan" do
+    it "migrate reserve data to ServiceTemplate" do
       st = service_template_stub.create!(:type => 'ServiceTemplateTransformationPlan')
+      reserve_stub.create!(
+        :resource_type => st.class.name,
+        :resource_id   => st.id,
+        :reserved      => {
+          :internal => true,
+        }
+      )
 
       migrate
 
       expect(st.reload.internal).to be_truthy
     end
 
-    it "skip internal for other types" do
-      st = service_template_stub.create!(:type => 'OtherServiceTemplateType')
+    it "migrate internal in ServiceTemplate to reserve table" do
+      st = service_template_stub.create!(:type => 'ServiceTemplateTransformationPlan')
 
       migrate
 
-      expect(st.reload.internal).to be_nil
+      expect(reserve_stub.count).to eq(1)
+
+      reserved_rec = reserve_stub.first
+      expect(reserved_rec.resource_id).to   eq(st.id)
+      expect(reserved_rec.resource_type).to eq(st.class.name)
+
+      expect(reserved_rec.reserved[:internal]).to be_truthy
     end
   end
 end
