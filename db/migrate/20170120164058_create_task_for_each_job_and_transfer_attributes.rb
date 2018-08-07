44ade48a30ce898b4d5a456fbcbd5f817d379133
Jason Frey <fryguy9@gmail.com>
2018-08-07 12:26:31 -0400

    Merge pull request #201 from yrudman/delete-old-job-while-creating-linked-task
    Optimize migration which creates task for each job
    (cherry picked from commit 8d48f1bedf9c42661ecb83ae3ab5ab9fb2b21545)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1618808

--- db/migrate/20170120164058_create_task_for_each_job_and_transfer_attributes.rb
+++ db/migrate/20170120164058_create_task_for_each_job_and_transfer_attributes.rb
@@ -8,7 +8,29 @@ class CreateTaskForEachJobAndTransferAttributes < ActiveRecord::Migration[5.0]
     has_one :job, :class_name => 'CreateTaskForEachJobAndTransferAttributes::Job'
   end
 
+  class LogFile < ActiveRecord::Base
+  end
+
+  class BinaryBlob < ActiveRecord::Base
+  end
+
   def up
+    purge_date = 7.days.ago.utc
+    say_with_time("Deleting finished jobs older than 7 days") do
+      Job.where("updated_on < ?", purge_date).where("state = 'finished'").delete_all
+    end
+
+    old_finished_tasks = MiqTask.where("updated_on < ?", purge_date).where("state = 'Finished'")
+    say_with_time("Deleting logs linked to finished tasks older than 7 days") do
+      LogFile.where(:miq_task_id => old_finished_tasks.select(:id)).delete_all
+    end
+    say_with_time("Deleting BinaryBlobs linked to finished tasks older than 7 days") do
+      BinaryBlob.where(:resource_type => 'MiqTask', :resource_id => old_finished_tasks.select(:id)).delete_all
+    end
+    say_with_time("Deleting finished tasks older than 7 days") do
+      old_finished_tasks.delete_all
+    end
+
     say_with_time("Creating tasks associated with jobs") do
       Job.find_each do |job|
         job.create_miq_task(:status        => job.status.try(:capitalize),
