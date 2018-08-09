9e66d3d0b05ee04d68c907532843f1c35049b874
Adam Grare <agrare@redhat.com>
2018-08-09 12:00:13 -0400

    Add version and concurrent tasks

--- db/migrate/20180807153714_add_conversion_host_table.rb
+++ db/migrate/20180807153714_add_conversion_host_table.rb
@@ -1,11 +1,13 @@
 class AddConversionHostTable < ActiveRecord::Migration[5.0]
   def change
     create_table :conversion_hosts do |t|
-      t.string :name
-      t.string :address
-      t.string :type
-      t.string :resource_type
-      t.bigint :resource_id
+      t.string  :name
+      t.string  :address
+      t.string  :type
+      t.string  :resource_type
+      t.bigint  :resource_id
+      t.string  :version
+      t.integer :max_concurrent_tasks
       t.timestamps
       t.index %w(resource_id resource_type)
     end
