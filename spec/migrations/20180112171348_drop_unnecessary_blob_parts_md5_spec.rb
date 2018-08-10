75770be114f3098bab4e8de1b896f0ef87b80167
Brandon Dunne <brandondunne@hotmail.com>
2018-08-10 17:50:05 -0400

    PAUSED: Use `git resume` to continue working.

--- spec/migrations/20180112171348_drop_unnecessary_blob_parts_md5_spec.rb
+++ spec/migrations/20180112171348_drop_unnecessary_blob_parts_md5_spec.rb
@@ -5,11 +5,11 @@ describe DropUnnecessaryBlobPartsMd5 do
 
   migration_context :down do
     it "restores the md5 sum and size of all records" do
-      binary_blob_part_stub.create!(:data => "A test string")
+      record = binary_blob_part_stub.create!(:data => "A test string")
 
       migrate
 
-      expect(binary_blob_part_stub.first).to have_attributes(
+      expect(record.reload).to have_attributes(
         :md5  => "99ce4dbdc6db1ab876443113ae24b816",
         :size => 13,
       )
