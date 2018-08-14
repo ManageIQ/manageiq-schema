8fc7012f983793a3289ab8197ad5266751c661ca
Adam Grare <agrare@redhat.com>
2018-08-14 10:09:48 -0400

    Use .references for polymorphic resource

--- db/migrate/20180807153714_add_conversion_host_table.rb
+++ db/migrate/20180807153714_add_conversion_host_table.rb
@@ -1,14 +1,13 @@
 class AddConversionHostTable < ActiveRecord::Migration[5.0]
   def change
     create_table :conversion_hosts do |t|
-      t.string  :name
-      t.string  :address
-      t.string  :type
-      t.string  :resource_type
-      t.bigint  :resource_id
-      t.string  :version
-      t.integer :max_concurrent_tasks
-      t.text    :capabilities
+      t.string     :name
+      t.string     :address
+      t.string     :type
+      t.references :resource, :type => :bigint, :polymorphic => true
+      t.string     :version
+      t.integer    :max_concurrent_tasks
+      t.text       :capabilities
       t.timestamps
       t.index %w(resource_id resource_type)
     end
