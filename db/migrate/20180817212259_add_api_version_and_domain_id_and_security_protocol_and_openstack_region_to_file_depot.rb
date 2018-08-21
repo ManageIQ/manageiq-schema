4cc441f7b098bbcb1f374e8fd02cb38afc05c3a1
Jerry Keselman <jkeselma@redhat.com>
2018-08-21 08:23:38 -0400

    rename domain_id since not numeric
    Spec tests expect all db columns ending in "_id" to be numeric.
    The keystone domain id is not numeric.  Rename the suffix to "_ident".

--- db/migrate/20180817212259_add_api_version_and_domain_id_and_security_protocol_and_openstack_region_to_file_depot.rb
+++ db/migrate/20180817212259_add_api_version_and_domain_id_and_security_protocol_and_openstack_region_to_file_depot.rb
@@ -1,7 +1,7 @@
 class AddApiVersionAndDomainIdAndSecurityProtocolAndOpenstackRegionToFileDepot < ActiveRecord::Migration[5.0]
   def change
     add_column :file_depots, :keystone_api_version, :string
-    add_column :file_depots, :v3_domain_id,         :string
+    add_column :file_depots, :v3_domain_ident,      :string
     add_column :file_depots, :security_protocol,    :string
     add_column :file_depots, :openstack_region,     :string
   end
