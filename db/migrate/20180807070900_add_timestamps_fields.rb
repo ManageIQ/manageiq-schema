ff1be27b502cceed9e2f91d1fd586ebf339754bf
Ladislav Smola <lsmola@redhat.com>
2018-08-23 13:38:27 +0200

    Rubocop issues

--- db/migrate/20180807070900_add_timestamps_fields.rb
+++ db/migrate/20180807070900_add_timestamps_fields.rb
@@ -19,24 +19,24 @@ class AddTimestampsFields < ActiveRecord::Migration[5.0]
     add_column :hardwares,                     :resource_timestamp, :datetime
     add_column :disks,                         :resource_timestamp, :datetime
 
-    add_column :availability_zones,            :resource_timestamps, :jsonb, default: {}
-    add_column :cloud_services,                :resource_timestamps, :jsonb, default: {}
-    add_column :cloud_tenants,                 :resource_timestamps, :jsonb, default: {}
-    add_column :cloud_object_store_containers, :resource_timestamps, :jsonb, default: {}
-    add_column :cloud_object_store_objects,    :resource_timestamps, :jsonb, default: {}
-    add_column :cloud_networks,                :resource_timestamps, :jsonb, default: {}
-    add_column :cloud_volumes,                 :resource_timestamps, :jsonb, default: {}
-    add_column :cloud_volume_backups,          :resource_timestamps, :jsonb, default: {}
-    add_column :cloud_volume_snapshots,        :resource_timestamps, :jsonb, default: {}
-    add_column :cloud_resource_quotas,         :resource_timestamps, :jsonb, default: {}
-    add_column :flavors,                       :resource_timestamps, :jsonb, default: {}
-    add_column :resource_groups,               :resource_timestamps, :jsonb, default: {}
-    add_column :host_aggregates,               :resource_timestamps, :jsonb, default: {}
-    add_column :orchestration_templates,       :resource_timestamps, :jsonb, default: {}
-    add_column :orchestration_stacks,          :resource_timestamps, :jsonb, default: {}
-    add_column :vms,                           :resource_timestamps, :jsonb, default: {}
-    add_column :hardwares,                     :resource_timestamps, :jsonb, default: {}
-    add_column :disks,                         :resource_timestamps, :jsonb, default: {}
+    add_column :availability_zones,            :resource_timestamps, :jsonb, :default => {}
+    add_column :cloud_services,                :resource_timestamps, :jsonb, :default => {}
+    add_column :cloud_tenants,                 :resource_timestamps, :jsonb, :default => {}
+    add_column :cloud_object_store_containers, :resource_timestamps, :jsonb, :default => {}
+    add_column :cloud_object_store_objects,    :resource_timestamps, :jsonb, :default => {}
+    add_column :cloud_networks,                :resource_timestamps, :jsonb, :default => {}
+    add_column :cloud_volumes,                 :resource_timestamps, :jsonb, :default => {}
+    add_column :cloud_volume_backups,          :resource_timestamps, :jsonb, :default => {}
+    add_column :cloud_volume_snapshots,        :resource_timestamps, :jsonb, :default => {}
+    add_column :cloud_resource_quotas,         :resource_timestamps, :jsonb, :default => {}
+    add_column :flavors,                       :resource_timestamps, :jsonb, :default => {}
+    add_column :resource_groups,               :resource_timestamps, :jsonb, :default => {}
+    add_column :host_aggregates,               :resource_timestamps, :jsonb, :default => {}
+    add_column :orchestration_templates,       :resource_timestamps, :jsonb, :default => {}
+    add_column :orchestration_stacks,          :resource_timestamps, :jsonb, :default => {}
+    add_column :vms,                           :resource_timestamps, :jsonb, :default => {}
+    add_column :hardwares,                     :resource_timestamps, :jsonb, :default => {}
+    add_column :disks,                         :resource_timestamps, :jsonb, :default => {}
 
     add_column :availability_zones,            :resource_timestamps_max, :datetime
     add_column :cloud_services,                :resource_timestamps_max, :datetime
