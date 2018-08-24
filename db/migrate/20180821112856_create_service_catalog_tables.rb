e45704c6b8ad744a731a3a59addb470529fdbfe1
Ladislav Smola <lsmola@redhat.com>
2018-08-24 08:58:04 +0200

    Remove container specific references
    Remove container specific references, since these services
    needs to be general

--- db/migrate/20180821112856_create_service_catalog_tables.rb
+++ db/migrate/20180821112856_create_service_catalog_tables.rb
@@ -1,14 +1,13 @@
-class CreateOpenshiftServiceCatalogTables < ActiveRecord::Migration[5.0]
+class CreateServiceCatalogTables < ActiveRecord::Migration[5.0]
   def change
-    create_table :container_service_classes, :id => :bigserial, :force => :cascade do |t|
+    create_table :service_classes, :id => :bigserial, :force => :cascade do |t|
       t.string :name
       t.string :ems_ref
       t.string :resource_version
+      t.string :type
       t.text :description
 
       t.references :ems, :type => :bigint, :index => true, :references => :ext_management_system
-      t.references :container_project, :type => :bigint, :index => true
-      t.references :container_service_broker, :type => :bigint, :index => true
 
       t.jsonb :extra
 
@@ -19,15 +18,15 @@ class CreateOpenshiftServiceCatalogTables < ActiveRecord::Migration[5.0]
       t.index :deleted_on
     end
 
-    create_table :container_service_plans, :id => :bigserial, :force => :cascade do |t|
+    create_table :service_plans, :id => :bigserial, :force => :cascade do |t|
       t.string :name
       t.string :ems_ref
       t.string :resource_version
+      t.string :type
       t.text :description
 
       t.references :ems, :type => :bigint, :index => true, :references => :ext_management_system
-      t.references :container_project, :type => :bigint, :index => true
-      t.references :container_service_class, :type => :bigint, :index => true
+      t.references :service_class, :type => :bigint, :index => true
 
       t.jsonb :extra
 
@@ -38,17 +37,15 @@ class CreateOpenshiftServiceCatalogTables < ActiveRecord::Migration[5.0]
       t.index :deleted_on
     end
 
-    create_table :container_service_instances, :id => :bigserial, :force => :cascade do |t|
+    create_table :service_instances, :id => :bigserial, :force => :cascade do |t|
       t.string :name
       t.string :ems_ref
-      t.string :secret_name
       t.string :resource_version
-      t.string :generate_name
+      t.string :type
 
       t.references :ems, :type => :bigint, :index => true, :references => :ext_management_system
-      t.references :container_project, :type => :bigint, :index => true
-      t.references :container_service_class, :type => :bigint, :index => {:name => 'csi_on_container_service_classes'}
-      t.references :container_service_plan, :type => :bigint, :index => true
+      t.references :service_class, :type => :bigint, :index => true
+      t.references :service_plan, :type => :bigint, :index => true
 
       t.jsonb :extra
 
