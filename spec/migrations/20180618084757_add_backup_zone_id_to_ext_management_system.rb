a73880e8a4ab0df3d55b796e3784850c8f013f8c
Martin Slemr <mslemr@redhat.com>
2018-09-20 11:01:51 +0200

    Maintenance zone support for suspending provider

---
+++ spec/migrations/20180618084757_add_backup_zone_id_to_ext_management_system.rb
@@ -0,0 +1,5 @@
+class AddBackupZoneIdToExtManagementSystem < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :ext_management_systems, :backup_zone, :type => :bigint, :index => true
+  end
+end
