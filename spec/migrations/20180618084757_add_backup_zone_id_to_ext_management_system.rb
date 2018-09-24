f315ccb5356735f487ab696a41d793781e61cceb
Martin Slemr <mslemr@redhat.com>
2018-09-24 18:39:24 +0200

    Renamed backup_zone to zone_before_pause migration

--- spec/migrations/20180618084757_add_backup_zone_id_to_ext_management_system.rb
+++
@@ -1,5 +0,0 @@
-class AddBackupZoneIdToExtManagementSystem < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :ext_management_systems, :backup_zone, :type => :bigint, :index => true
-  end
-end
