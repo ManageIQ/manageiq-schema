7c3eb638eb276ca0bb7eba6570f22e9d87a10eb7
Adam Grare <agrare@redhat.com>
2018-10-02 13:55:21 -0400

    Address review comments about specs

--- spec/migrations/20181001131632_add_conversion_host_id_to_miq_request_tasks_spec.rb
+++ spec/migrations/20181001131632_add_conversion_host_id_to_miq_request_tasks_spec.rb
@@ -6,7 +6,7 @@ describe AddConversionHostIdToMiqRequestTasks do
   let(:conversion_host_stub) { migration_stub(:ConversionHost) }
 
   migration_context :up do
-    it "when host doesn't exist" do
+    it "doesn't set the conversion host when the host doesn't exists" do
       host = host_stub.create!
       conversion_host_id = host.id
       task = task_stub.create!(
@@ -19,11 +19,11 @@ describe AddConversionHostIdToMiqRequestTasks do
       task.reload
 
       expect(task.options).to eq(:dummy_key => 'dummy_value')
-      expect(AddConversionHostIdToMiqRequestTasks::ConversionHost.find_by(:resource_id => conversion_host_id)).to be_nil
+      expect(conversion_host_stub.find_by(:resource_id => conversion_host_id)).to be_nil
       expect(task.conversion_host).to be_nil
     end
 
-    it "when host exist" do
+    it "creates a conversion host and updates the task when the host exists" do
       host = host_stub.create!
       task = task_stub.create!(
         :type    => 'ServiceTemplateTransformationPlanTask',
@@ -34,8 +34,8 @@ describe AddConversionHostIdToMiqRequestTasks do
       task.reload
 
       expect(task.options).to eq(:dummy_key => 'dummy_value')
-      expect(AddConversionHostIdToMiqRequestTasks::ConversionHost.find_by(:resource => host)).not_to be_nil
-      expect(task.conversion_host).to eq(AddConversionHostIdToMiqRequestTasks::ConversionHost.find_by(:resource => host))
+      expect(conversion_host_stub.find_by(:resource => host)).not_to be_nil
+      expect(task.conversion_host).to eq(conversion_host_stub.find_by(:resource => host))
     end
   end
 
