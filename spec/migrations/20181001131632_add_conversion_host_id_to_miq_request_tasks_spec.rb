3a6c8454a2d552cea69f4c7802486890a3d96cca
Fabien Dupont <fdupont@redhat.com>
2018-10-02 12:31:13 -0400

    Add test when host is nil. Add dummy key to task.options

--- spec/migrations/20181001131632_add_conversion_host_id_to_miq_request_tasks_spec.rb
+++ spec/migrations/20181001131632_add_conversion_host_id_to_miq_request_tasks_spec.rb
@@ -4,19 +4,34 @@ describe AddConversionHostIdToMiqRequestTasks do
   let(:task_stub) { migration_stub(:MiqRequestTask) }
   let(:host_stub) { migration_stub(:Host) }
   let(:conversion_host_stub) { migration_stub(:ConversionHost) }
+  let(:host_id) { 42 }
 
   migration_context :up do
-    it "creates conversion host" do
-      host = host_stub.create!
+    it "when host doesn't exist" do
       task = task_stub.create!(
         :type    => 'ServiceTemplateTransformationPlanTask',
-        :options => { :transformation_host_id => host.id }
+        :options => { :dummy_key => 'dummy_value', :transformation_host_id => host_id }
       )
 
       migrate
-
       task.reload
-      expect(task.options).to eq({})
+
+      expect(task.options).to eq({ :dummy_key => 'dummy_value', :transformation_host_id => host_id })
+      expect(AddConversionHostIdToMiqRequestTasks::ConversionHost.find_by(:resource_id => host_id)).to be_nil
+      expect(task.conversion_host).to be_nil
+    end
+
+    it "when host exist" do
+      task = task_stub.create!(
+        :type    => 'ServiceTemplateTransformationPlanTask',
+        :options => { :dummy_key => 'dummy_value', :transformation_host_id => host_id }
+      )
+      host = host_stub.create!(:id => host_id)
+
+      migrate
+      task.reload
+
+      expect(task.options).to eq({ :dummy_key => 'dummy_value' })
       expect(AddConversionHostIdToMiqRequestTasks::ConversionHost.find_by(:resource => host)).not_to be_nil
       expect(task.conversion_host).to eq(AddConversionHostIdToMiqRequestTasks::ConversionHost.find_by(:resource => host))
     end
@@ -25,9 +40,7 @@ describe AddConversionHostIdToMiqRequestTasks do
   migration_context :down do
     it "updates task.options" do
       host = host_stub.create!
-      conversion_host = conversion_host_stub.create!(
-        :resource => host
-      )
+      conversion_host = conversion_host_stub.create!(:resource => host)
       task = task_stub.create!(
         :type            => 'ServiceTemplateTransformationPlanTask',
         :conversion_host => conversion_host
