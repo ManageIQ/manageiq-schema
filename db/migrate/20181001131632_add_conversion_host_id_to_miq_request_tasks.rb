4f6de5750c35806e066449ec9e6dca7c230c65f0
Adam Grare <agrare@redhat.com>
2018-10-02 16:19:24 -0400

    Update previous migration to scope to current region

--- db/migrate/20181001131632_add_conversion_host_id_to_miq_request_tasks.rb
+++ db/migrate/20181001131632_add_conversion_host_id_to_miq_request_tasks.rb
@@ -1,6 +1,8 @@
 class AddConversionHostIdToMiqRequestTasks < ActiveRecord::Migration[5.0]
   class MiqRequestTask < ActiveRecord::Base
     self.inheritance_column = :_type_disabled
+    include ActiveRecord::IdRegions
+
     serialize :options, Hash
     belongs_to :conversion_host, :class_name => "AddConversionHostIdToMiqRequestTasks::ConversionHost"
   end
@@ -17,7 +19,7 @@ class AddConversionHostIdToMiqRequestTasks < ActiveRecord::Migration[5.0]
   def up
     add_column :miq_request_tasks, :conversion_host_id, :bigint
 
-    MiqRequestTask.where(:type => 'ServiceTemplateTransformationPlanTask').each do |task|
+    MiqRequestTask.in_my_region.where(:type => 'ServiceTemplateTransformationPlanTask').find_each do |task|
       host_id = task.options.delete(:transformation_host_id)
       next unless host_id
       host = Host.find_by(:id => host_id)
@@ -33,12 +35,13 @@ class AddConversionHostIdToMiqRequestTasks < ActiveRecord::Migration[5.0]
   end
 
   def down
-    conversion_host_ids = MiqRequestTask.where(:type => 'ServiceTemplateTransformationPlanTask').map do |task|
+    conversion_host_ids = MiqRequestTask.in_my_region.where(:type => 'ServiceTemplateTransformationPlanTask').map do |task|
       next if task.conversion_host.nil?
       task.options[:transformation_host_id] = task.conversion_host.resource.id
       task.save!
       task.conversion_host.id
     end.uniq.compact
+
     ConversionHost.destroy(conversion_host_ids)
 
     remove_column :miq_request_tasks, :conversion_host_id
