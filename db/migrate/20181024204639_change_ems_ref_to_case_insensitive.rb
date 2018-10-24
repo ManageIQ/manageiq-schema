6edf90ff51193e536d8973c15cb3c09a77625a04
Daniel Berger <dberger@redhat.com>
2018-10-24 15:02:56 -0600

    Use up/down instead of change.

--- db/migrate/20181024204639_change_ems_ref_to_case_insensitive.rb
+++ db/migrate/20181024204639_change_ems_ref_to_case_insensitive.rb
@@ -1,6 +1,10 @@
 class ChangeEmsRefToCaseInsensitive < ActiveRecord::Migration[5.0]
-  def change
+  def up
     enable_extension :citext
     change_column :vms, :ems_ref, :citext
+  end
+
+  def down
+    disable_extension :citext
   end
 end
