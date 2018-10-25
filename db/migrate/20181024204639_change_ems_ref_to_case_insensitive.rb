6708cb33be953d96bfd5bd3b763f41579f78838f
Daniel Berger <dberger@redhat.com>
2018-10-25 09:06:25 -0600

    Add more columns, update down method.

--- db/migrate/20181024204639_change_ems_ref_to_case_insensitive.rb
+++ db/migrate/20181024204639_change_ems_ref_to_case_insensitive.rb
@@ -1,10 +1,38 @@
 class ChangeEmsRefToCaseInsensitive < ActiveRecord::Migration[5.0]
   def up
     enable_extension :citext
+    change_column :availability_zones, :ems_ref, :citext
+    change_column :cloud_networks, :ems_ref, :citext
+    change_column :cloud_subnets, :ems_ref, :citext
+    change_column :cloud_volumes, :ems_ref, :citext
+    change_column :flavors, :ems_ref, :citext
+    change_column :floating_ips, :ems_ref, :citext
+    change_column :load_balancers, :ems_ref, :citext
+    change_column :load_balancer_health_checks, :ems_ref, :citext
+    change_column :load_balancer_listeners, :ems_ref, :citext
+    change_column :load_balancer_pools, :ems_ref, :citext
+    change_column :load_balancer_pool_members, :ems_ref, :citext
+    change_column :network_groups, :ems_ref, :citext
+    change_column :network_ports, :ems_ref, :citext
+    change_column :network_routers, :ems_ref, :citext
     change_column :vms, :ems_ref, :citext
   end
 
   def down
-    disable_extension :citext
+    change_column :availability_zones, :ems_ref, :text
+    change_column :cloud_networks, :ems_ref, :text
+    change_column :cloud_subnets, :ems_ref, :text
+    change_column :cloud_volumes, :ems_ref, :text
+    change_column :flavors, :ems_ref, :text
+    change_column :floating_ips, :ems_ref, :text
+    change_column :load_balancers, :ems_ref, :text
+    change_column :load_balancer_health_checks, :ems_ref, :text
+    change_column :load_balancer_listeners, :ems_ref, :text
+    change_column :load_balancer_pools, :ems_ref, :text
+    change_column :load_balancer_pool_members, :ems_ref, :text
+    change_column :network_groups, :ems_ref, :text
+    change_column :network_ports, :ems_ref, :text
+    change_column :network_routers, :ems_ref, :text
+    change_column :vms, :ems_ref, :text
   end
 end
