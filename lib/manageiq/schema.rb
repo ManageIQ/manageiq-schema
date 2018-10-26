d058a0ea21811520ead82347fd1b90c7cfe852c4
Adam Grare <agrare@redhat.com>
2018-10-26 11:38:23 -0400

    Switch to manageiq-password instead of gems-pending
    Use the extracted manageiq-password gem instead of gems-pending

--- lib/manageiq/schema.rb
+++ lib/manageiq/schema.rb
@@ -2,7 +2,7 @@ require "manageiq/schema/version"
 require "manageiq/schema/engine"
 
 require 'activerecord-id_regions'
-require 'gems/pending/util/miq-password'
+require 'manageiq/password'
 require 'more_core_extensions/all' # TODO: Move this into specific migrations that need it
 
 require 'migration_helper'
