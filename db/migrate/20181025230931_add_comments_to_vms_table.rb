e86eba5217c752f85c68327d75cec23ff954e4ee
Daniel Berger <dberger@redhat.com>
2018-10-29 11:11:38 -0600

    Updated description field.

--- db/migrate/20181025230931_add_comments_to_vms_table.rb
+++ db/migrate/20181025230931_add_comments_to_vms_table.rb
@@ -27,7 +27,7 @@ class AddCommentsToVmsTable < ActiveRecord::Migration[5.0]
     change_column_comment :vms, :cpu_shares_level, "CPU shares allocation level. VMWare only."
     change_column_comment :vms, :created_on, "The timestamp the VM was added to the app inventory."
     change_column_comment :vms, :deprecated, "Indicates whether or not a template is deprecated. Google only."
-    change_column_comment :vms, :description, "A custom description that users can add within the app."
+    change_column_comment :vms, :description, "A description of the VM that is typically set by the provider, but may also be edited."
     change_column_comment :vms, :ems_created_on, "Date the associated EMS was created."
     change_column_comment :vms, :ems_id, "ID of the ExtManagementSystem associated with the VM."
     change_column_comment :vms, :evm_owner_id, "ID of VM owner from product database."
