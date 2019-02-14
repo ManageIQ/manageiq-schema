e5d8e23ba68999833e0dcf01447a686af8920b61
Daniel Berger <dberger@redhat.com>
2019-02-14 10:27:11 -0500

    Update identifier field.

--- db/migrate/20190214125845_add_comments_to_miq_tasks_table.rb
+++ db/migrate/20190214125845_add_comments_to_miq_tasks_table.rb
@@ -5,7 +5,7 @@ class AddCommentsToMiqTasksTable < ActiveRecord::Migration[5.0]
     change_column_comment :miq_tasks, :context_data, "Serialized metadata that may contain more detailed information about the task."
     change_column_comment :miq_tasks, :created_on, "The timestamp the task was added to the app inventory."
     change_column_comment :miq_tasks, :id, "The internal database ID."
-    change_column_comment :miq_tasks, :identifier, "The identifier used by metrics rollups for EMS clusters."
+    change_column_comment :miq_tasks, :identifier, "The resource this task is related to in '#{klass}:#{id}' format."
     change_column_comment :miq_tasks, :message, "Message that describes the reason for the current status."
     change_column_comment :miq_tasks, :miq_server_id, "ID of the ManageIQ server that the task belongs to."
     change_column_comment :miq_tasks, :name, "The name of the task. Typically doubles as a description."
