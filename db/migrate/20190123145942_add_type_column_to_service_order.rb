9ff741321b59c410eba4201c98005b18872165a1
Daniel Berger <dberger@redhat.com>
2019-03-19 14:22:24 -0400

    Change column from type to request_type, and use joined string as result.

--- db/migrate/20190123145942_add_type_column_to_service_order.rb
+++ db/migrate/20190123145942_add_type_column_to_service_order.rb
@@ -1,15 +1,16 @@
 class AddTypeColumnToServiceOrder < ActiveRecord::Migration[5.0]
   def up
-    add_column :service_order, :type, :string
+    add_column :service_order, :request_type, :string
 
-    say_with_time("Set service_order type") do
+    say_with_time("Set service_order request_type") do
       ServiceOrder.all.each do |service_order|
-        service_order.update_attribute(:type, service_order.miq_requests.pluck(:type))
+        request_type = service_order.miq_requests.pluck(:type).uniq.sort.join(',')
+        service_order.update_attribute(:request_type, request_type)
       end
     end
   end
 
   def down
-    remove_column :service_order, :type
+    remove_column :service_order, :request_type
   end
 end
