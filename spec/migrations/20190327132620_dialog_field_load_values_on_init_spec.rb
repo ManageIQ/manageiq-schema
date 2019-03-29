2c9898443598b95e9eea222f1b28b044fcf5a46b
Martin Hradil <mhradil@redhat.com>
2019-03-29 15:52:12 +0000

    20190327132620_dialog_field_load_values_on_init spec - make sure the order fits, don't use keywords

--- spec/migrations/20190327132620_dialog_field_load_values_on_init_spec.rb
+++ spec/migrations/20190327132620_dialog_field_load_values_on_init_spec.rb
@@ -5,9 +5,9 @@ describe DialogFieldLoadValuesOnInit do
 
   migration_context :up do
     before do
-      [true, false, nil].each do |show|
-        [true, false, nil].each do |load|
-          dialog_field_stub.create!(:show_refresh_button => show, :load_values_on_init => load)
+      [true, false, nil].each do |show_button|
+        [true, false, nil].each do |load_values|
+          dialog_field_stub.create!(:show_refresh_button => show_button, :load_values_on_init => load_values)
         end
       end
 
@@ -15,7 +15,11 @@ describe DialogFieldLoadValuesOnInit do
     end
 
     it "updates load_values_on_init" do
-      expect(dialog_field_stub.pluck(:load_values_on_init)).to eq([true, false, nil, true, true, true, true, true, true])
+      expect(dialog_field_stub.sort_by(:id).pluck(:load_values_on_init)).to eq([
+        true, false, nil, # show_button was true
+        true, true, true,
+        true, true, true,
+      ])
     end
   end
 end
