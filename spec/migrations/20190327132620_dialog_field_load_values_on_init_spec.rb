0e6f406c10c3bbd2850e737dcb4e436e7c74c597
Martin Hradil <mhradil@redhat.com>
2019-03-29 16:14:40 +0000

    dialog_field_load_values_on_init spec - use explicit contexts for the three possibilities

--- spec/migrations/20190327132620_dialog_field_load_values_on_init_spec.rb
+++ spec/migrations/20190327132620_dialog_field_load_values_on_init_spec.rb
@@ -5,21 +5,35 @@ describe DialogFieldLoadValuesOnInit do
 
   migration_context :up do
     before do
-      [true, false, nil].each do |show_button|
-        [true, false, nil].each do |load_values|
-          dialog_field_stub.create!(:show_refresh_button => show_button, :load_values_on_init => load_values)
-        end
+      [true, false, nil].each do |load_values|
+        dialog_field_stub.create!(:show_refresh_button => show_button, :load_values_on_init => load_values)
       end
 
       migrate
     end
 
-    it "updates load_values_on_init" do
-      expect(dialog_field_stub.sort_by(:id).pluck(:load_values_on_init)).to eq([
-        true, false, nil, # show_button was true
-        true, true, true,
-        true, true, true,
-      ])
+    context "when show_refresh_button is true" do
+      let(:show_button) { true }
+
+      it "doesnt update load_values_on_init" do
+        expect(dialog_field_stub.order(:id).pluck(:load_values_on_init)).to eq([true, false, nil])
+      end
+    end
+
+    context "when show_refresh_button is false" do
+      let(:show_button) { false }
+
+      it "updates load_values_on_init to true" do
+        expect(dialog_field_stub.order(:id).pluck(:load_values_on_init)).to eq([true, true, true])
+      end
+    end
+
+    context "when show_refresh_button is nil" do
+      let(:show_button) { nil }
+
+      it "updates load_values_on_init to true" do
+        expect(dialog_field_stub.order(:id).pluck(:load_values_on_init)).to eq([true, true, true])
+      end
     end
   end
 end
