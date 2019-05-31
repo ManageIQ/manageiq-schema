c7f349455cc22121dadaff79829ff1fa50114ff7
Jason Frey <jfrey@redhat.com>
2019-05-31 12:24:57 -0400

    Add guid to miq_databases
    It turns out the only real "product-wide" guid we have is in
    miq_regions, but the problem with miq_regions is that it's replicated,
    so it's slightly complicated to find my_region.guid.  Instead, let's
    have a miq_databases.guid.

---
+++ db/migrate/20190531161732_add_guid_to_miq_databases.rb
@@ -0,0 +1,16 @@
+class AddGuidToMiqDatabases < ActiveRecord::Migration[5.2]
+  class MiqDatabase < ActiveRecord::Base
+  end
+
+  def up
+    add_column :miq_databases, :guid, :string, :limit => 36
+
+    say_with_time("Create default guids for miq_databases") do
+      MiqDatabase.all.each { |d| d.update!(:guid => SecureRandom.uuid) }
+    end
+  end
+
+  def down
+    remove_column :miq_databases, :guid
+  end
+end
