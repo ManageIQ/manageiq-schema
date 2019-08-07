ed95ecf9b91de89c4de30702d2efc81ba0a5438d
Brandon Dunne <bdunne@redhat.com>
2019-08-07 12:01:45 -0400

    Merge pull request #401 from jrafanie/migrate_with_cleared_schema_cache
    Migrate with cleared schema cache
    
    (cherry picked from commit 44bbd51ddb5a8566ebe5c75da4c4175d726e0774)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1732808

--- lib/manageiq/schema/engine.rb
+++ lib/manageiq/schema/engine.rb
@@ -3,6 +3,11 @@ module ManageIQ
     class Engine < ::Rails::Engine
       isolate_namespace ManageIQ::Schema
 
+      ActiveSupport.on_load(:active_record) do
+        require_relative 'migrate_with_cleared_schema_cache'
+        ActiveRecord::Migration.prepend(MigrateWithClearedSchemaCache)
+      end
+
       initializer :append_migrations do |app|
         unless app.root.to_s.match root.to_s
           config.paths["db/migrate"].expanded.each do |expanded_path|
