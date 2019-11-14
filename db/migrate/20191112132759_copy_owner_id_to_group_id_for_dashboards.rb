927a13d3e19cb1a592594e218f134924b24deb43
Yuri Rudman <yrudman@redhat.com>
2019-11-14 11:36:17 -0500

    optimized migration by using update_all

--- db/migrate/20191112132759_copy_owner_id_to_group_id_for_dashboards.rb
+++ db/migrate/20191112132759_copy_owner_id_to_group_id_for_dashboards.rb
@@ -3,17 +3,13 @@ class CopyOwnerIdToGroupIdForDashboards < ActiveRecord::Migration[5.1]
 
   def up
     say_with_time("Copying owner_id to group_id in miq_sets table for each dashboard assigned to group") do
-      MiqSet.where(:set_type => "MiqWidgetSet", :owner_type => "MiqGroup").find_each do |dashboard|
-        dashboard.update(:group_id => dashboard.owner_id)
-      end
+      MiqSet.where(:set_type => "MiqWidgetSet", :owner_type => "MiqGroup").update_all("group_id = owner_id")
     end
   end
 
   def down
     say_with_time("Nullifying group_id column in miw_sets table for each dashboard assigned to group") do
-      MiqSet.where(:set_type => "MiqWidgetSet", :owner_type => "MiqGroup").find_each do |dashboard|
-        dashboard.update(:group_id => nil)
-      end
+      MiqSet.where(:set_type => "MiqWidgetSet", :owner_type => "MiqGroup").update_all(:group_id => nil)
     end
   end
 end
