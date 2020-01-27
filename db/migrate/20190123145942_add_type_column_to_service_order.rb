da0823d6e75f6b9d02a3c757da7af29003407d3c
Daniel Berger <djberg96@gmail.com>
2020-01-27 08:52:48 -0500

    Switch back to STI.

--- db/migrate/20190123145942_add_type_column_to_service_order.rb
+++ db/migrate/20190123145942_add_type_column_to_service_order.rb
@@ -1,16 +1,16 @@
 class AddTypeColumnToServiceOrder < ActiveRecord::Migration[5.0]
   def up
-    add_column :service_order, :request_type, :string
+    add_column :service_order, :type, :string
 
-    say_with_time("Set service_order request_type") do
+    say_with_time("Set service_order type") do
       ServiceOrder.all.each do |service_order|
-        request_type = service_order.miq_requests.pluck(:type).uniq.sort.join(',')
-        service_order.update_attribute(:request_type, request_type)
+        type = service_order.miq_requests.pluck(:type).any? { |t| t === 'ServiceTemplateTransformationPlanRequest' } ? ServiceOrderV2V : ServiceOrderCart
+        service_order.update_attribute(:type, type)
       end
     end
   end
 
   def down
-    remove_column :service_order, :request_type
+    remove_column :service_order, :type
   end
 end
