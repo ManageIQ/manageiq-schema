8657e804125199bd86b059a3db8f3e9e2e83efe7
Adam Grare <agrare@redhat.com>
2020-01-30 13:09:25 -0500

    Ensure that constants get restored

--- db/migrate/20200126203714_remove_vim_types_from_ems_events.rb
+++ db/migrate/20200126203714_remove_vim_types_from_ems_events.rb
@@ -33,30 +33,31 @@ class RemoveVimTypesFromEmsEvents < ActiveRecord::Migration[5.1]
   end
 
   def up
-    vim_hash_backup   = const_replace(:VimHash, VimHash)
-    vim_string_backup = const_replace(:VimString, VimString)
-    vim_array_backup  = const_replace(:VimArray, VimArray)
+    with_replaced_constants(:VimHash => VimHash, :VimString => VimString, :VimArray => VimArray) do
+      say_with_time("Removing Vim Types from EmsEvents") do
+        base_relation = EventStream.in_my_region.where(:source => "VC")
+        say_batch_started(base_relation.size)
 
-    say_with_time("Removing Vim Types from EmsEvents") do
-      base_relation = EventStream.in_my_region.where(:source => "VC")
-      say_batch_started(base_relation.size)
+        processed_count = 0
+        base_relation.find_each(batch_size: BATCH_SIZE) do |event|
+          full_data = YAML.load(event.full_data)
+          event.update!(:full_data => vim_types_to_basic_types(full_data).to_yaml)
 
-      processed_count = 0
-      base_relation.find_each(batch_size: BATCH_SIZE) do |event|
-        full_data = YAML.load(event.full_data)
-        event.update!(:full_data => vim_types_to_basic_types(full_data).to_yaml)
-
-        processed_count += 1
-        say_batch_processed(processed_count) if processed_count % BATCH_SIZE == 0
+          processed_count += 1
+          say_batch_processed(processed_count) if processed_count % BATCH_SIZE == 0
+        end
       end
     end
-
-    const_replace(:VimHash, vim_hash_backup)
-    const_replace(:VimString, vim_string_backup)
-    const_replace(:VimArray, vim_array_backup)
   end
 
   private
+
+  def with_replaced_constants(constants)
+    backed_up_constants = Hash[constants.map { |sym, klass| [sym, const_replace(sym, klass)] }]
+    yield
+  ensure
+    backed_up_constants.each { |sym, klass| const_replace(sym, klass) }
+  end
 
   def const_replace(const_name, new_const)
     old_const = const_remove(const_name)
@@ -66,7 +67,6 @@ class RemoveVimTypesFromEmsEvents < ActiveRecord::Migration[5.1]
 
   def const_remove(const_name)
     return unless Object.const_defined?(const_name)
-    old_const = Object.const_get(const_name)
     Object.send(:remove_const, const_name)
   end
 
