95f19b75119b40a177f77111dabe9a736576a3af
Adam Grare <agrare@redhat.com>
2020-02-11 16:04:34 -0500

    Use regex string replace instead of yaml load

--- db/migrate/20200126203714_remove_vim_types_from_ems_events.rb
+++ db/migrate/20200126203714_remove_vim_types_from_ems_events.rb
@@ -1,31 +1,4 @@
 class RemoveVimTypesFromEmsEvents < ActiveRecord::Migration[5.1]
-  disable_ddl_transaction!
-  include MigrationHelper
-
-  BATCH_SIZE = 5_000
-
-  module VimType
-    def vimType
-      @vimType.nil? ? nil : @vimType.to_s
-    end
-
-    def vimType=(val)
-      @vimType = val.nil? ? nil : val.to_sym
-    end
-
-    def xsiType
-      @xsiType.nil? ? nil : @xsiType.to_s
-    end
-
-    def xsiType=(val)
-      @xsiType = val.nil? ? nil : val.to_sym
-    end
-  end
-
-  class VimHash < Hash; include VimType; end
-  class VimArray < Array; include VimType; end
-  class VimString < String; include VimType; end
-
   class EventStream < ActiveRecord::Base
     include ActiveRecord::IdRegions
 
@@ -33,72 +6,10 @@ class RemoveVimTypesFromEmsEvents < ActiveRecord::Migration[5.1]
   end
 
   def up
-    with_replaced_constants(:VimHash => VimHash, :VimString => VimString, :VimArray => VimArray) do
-      say_with_time("Removing Vim Types from EmsEvents") do
-        base_relation = EventStream.in_my_region.where(:source => "VC").where("full_data LIKE ?", "%hash-with-ivars:VimHash%")
-        say_batch_started(base_relation.size)
-
-        base_relation.find_in_batches(batch_size: BATCH_SIZE) do |events|
-          processors = ENV["RAILS_ENV"] == "test" ? 0 : nil
-
-          resources = events.collect { |event| [event.id, event.full_data] }
-
-          require 'parallel'
-          resources = Parallel.map(resources, :in_processes => processors) do |event_id, full_data|
-            ActiveRecord::Base.connection.reconnect! unless processors == 0
-
-            [event_id, vim_types_to_basic_types(YAML.load(full_data)).to_yaml]
-          end
-
-          ActiveRecord::Base.connection.reconnect! unless processors == 0
-
-          event_full_data_by_event_id = Hash[resources]
-
-          ActiveRecord::Base.transaction do
-            events.each do |event|
-              event.update!(:full_data => event_full_data_by_event_id[event.id])
-            end
-          end
-
-          say_batch_processed(events.count)
-        end
-      end
+    say_with_time("Removing Vim Types from EmsEvents") do
+      EventStream.in_my_region.where(:source => "VC")
+        .where("full_data LIKE ?", "%hash-with-ivars:VimHash%")
+        .update_all("full_data = REGEXP_REPLACE(full_data, '!ruby/(string|array|hash-with-ivars):Vim(Hash|String|Array)', '!ruby/\\1:\\2', 'g')")
     end
-  end
-
-  private
-
-  def with_replaced_constants(constants)
-    backed_up_constants = Hash[constants.map { |sym, klass| [sym, const_replace(sym, klass)] }]
-    yield
-  ensure
-    backed_up_constants.each { |sym, klass| const_replace(sym, klass) unless klass.nil? }
-  end
-
-  def const_replace(const_name, new_const)
-    old_const = const_remove(const_name)
-    Object.const_set(const_name, new_const)
-    old_const
-  end
-
-  def const_remove(const_name)
-    return unless Object.const_defined?(const_name)
-
-    Object.send(:remove_const, const_name)
-  end
-
-  def vim_types_to_basic_types(obj)
-    case obj
-    when VimString
-      obj = obj.to_s
-    when VimHash
-      obj = obj.to_h
-      obj.transform_values! { |val| vim_types_to_basic_types(val) }
-    when VimArray
-      obj = obj.to_a
-      obj.map! { |v| vim_types_to_basic_types(v) }
-    end
-
-    obj
   end
 end
