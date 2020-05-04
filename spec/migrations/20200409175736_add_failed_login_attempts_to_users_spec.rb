98a335332ca9eb25a933623d1f31f276138682da
Jason Frey <fryguy9@gmail.com>
2020-05-04 16:10:20 -0400

    Merge pull request #467 from skateman/add-failed-login
    Add new failed_login_attempts column to the Users table
    
    (cherry picked from commit 6271b8e1176c9de5a6475cd406b164c4b72ba24a)

---
+++ spec/migrations/20200409175736_add_failed_login_attempts_to_users_spec.rb
@@ -0,0 +1,17 @@
+require_migration
+
+describe AddFailedLoginAttemptsToUsers do
+  let(:user_stub) { migration_stub(:User) }
+  let(:user) { user_stub.create(:name => 'test', :userid => 'test') }
+
+  migration_context :up do
+    it 'creates a new field which defaults to 0' do
+      expect { user.failed_login_attempts }.to raise_error
+
+      migrate
+      user.reload
+
+      expect(user.failed_login_attempts).to eq(0)
+    end
+  end
+end
