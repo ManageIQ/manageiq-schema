rails/rails

commit f185e0ebc9cc18c596eb9cab1a9cd99e26a20500

Except `table_name` from column objects
The `table_name` was added at #23677 to detect whether serial column or
not correctly.

We can do that detection before initialize column object, it makes
column object size smaller, and it probably helps column object
de-duplication.

activerecord/lib/active_record/connection_adapters/mysql/schema_dumper.rb
@@ -55,7 +55,7 @@ module ActiveRecord
           end
 
           def schema_collation(column)
-            if column.collation && table_name = column.table_name
+            if column.collation
               @table_collation_cache ||= {}
               @table_collation_cache[table_name] ||=
                 @connection.exec_query("SHOW TABLE STATUS LIKE #{@connection.quote(table_name)}", "SCHEMA").first["Collation"]
@@ -65,13 +65,13 @@ module ActiveRecord
 
           def extract_expression_for_virtual_column(column)
             if @connection.mariadb? && @connection.database_version < "10.2.5"
-              create_table_info = @connection.send(:create_table_info, column.table_name)
+              create_table_info = @connection.send(:create_table_info, table_name)
               column_name = @connection.quote_column_name(column.name)
               if %r/#{column_name} #{Regexp.quote(column.sql_type)}(?: COLLATE \w+)? AS \((?<expression>.+?)\) #{column.extra}/ =~ create_table_info
                 $~[:expression].inspect
               end
             else
-              scope = @connection.send(:quoted_scope, column.table_name)
+              scope = @connection.send(:quoted_scope, table_name)
               column_name = @connection.quote(column.name)
               sql = "SELECT generation_expression FROM information_schema.columns" \
                     " WHERE table_schema = #{scope[:schema]}" \

activerecord/lib/active_record/schema_dumper.rb
@@ -47,6 +47,7 @@ module ActiveRecord
     end
 
     private
+      attr_accessor :table_name
 
       def initialize(connection, options = {})
         @connection = connection
@@ -110,6 +111,8 @@ HEADER
       def table(table, stream)
         columns = @connection.columns(table)
         begin
+          self.table_name = table
+
           tbl = StringIO.new
 
           # first dump primary key column
@@ -159,6 +162,8 @@ HEADER
           stream.puts "# Could not dump table #{table.inspect} because of following #{e.class}"
           stream.puts "#   #{e.message}"
           stream.puts
+        ensure
+          self.table_name = nil
         end
       end
 

activerecord/lib/active_record/connection_adapters/column.rb
@@ -5,7 +5,7 @@ module ActiveRecord
   module ConnectionAdapters
     # An abstract definition of a column in a table.
     class Column
-      attr_reader :name, :default, :sql_type_metadata, :null, :table_name, :default_function, :collation, :comment
+      attr_reader :name, :default, :sql_type_metadata, :null, :default_function, :collation, :comment
 
       delegate :precision, :scale, :limit, :type, :sql_type, to: :sql_type_metadata, allow_nil: true
 
@@ -15,9 +15,8 @@ module ActiveRecord
       # +default+ is the type-casted default value, such as +new+ in <tt>sales_stage varchar(20) default 'new'</tt>.
       # +sql_type_metadata+ is various information about the type of the column
       # +null+ determines if this column allows +NULL+ values.
-      def initialize(name, default, sql_type_metadata = nil, null = true, table_name = nil, default_function = nil, collation = nil, comment: nil, **)
+      def initialize(name, default, sql_type_metadata = nil, null = true, default_function = nil, collation: nil, comment: nil, **)
         @name = name.freeze
-        @table_name = table_name
         @sql_type_metadata = sql_type_metadata
         @null = null
         @default = default
@@ -44,7 +43,6 @@ module ActiveRecord
 
       def init_with(coder)
         @name = coder["name"]
-        @table_name = coder["table_name"]
         @sql_type_metadata = coder["sql_type_metadata"]
         @null = coder["null"]
         @default = coder["default"]
@@ -55,7 +53,6 @@ module ActiveRecord
 
       def encode_with(coder)
         coder["name"] = @name
-        coder["table_name"] = @table_name
         coder["sql_type_metadata"] = @sql_type_metadata
         coder["null"] = @null
         coder["default"] = @default
@@ -77,7 +74,7 @@ module ActiveRecord
       protected
 
         def attributes_for_hash
-          [self.class, name, default, sql_type_metadata, null, table_name, default_function, collation]
+          [self.class, name, default, sql_type_metadata, null, default_function, collation, comment]
         end
     end
 

activerecord/lib/active_record/connection_adapters/mysql/schema_statements.rb
@@ -174,9 +174,8 @@ module ActiveRecord
               default,
               type_metadata,
               field[:Null] == "YES",
-              table_name,
               default_function,
-              field[:Collation],
+              collation: field[:Collation],
               comment: field[:Comment].presence
             )
           end

activerecord/lib/active_record/connection_adapters/postgresql/column.rb
@@ -2,42 +2,21 @@
 
 module ActiveRecord
   module ConnectionAdapters
-    # PostgreSQL-specific extensions to column definitions in a table.
-    class PostgreSQLColumn < Column #:nodoc:
-      delegate :array, :oid, :fmod, to: :sql_type_metadata
-      alias :array? :array
+    module PostgreSQL
+      class Column < ConnectionAdapters::Column # :nodoc:
+        delegate :array, :oid, :fmod, to: :sql_type_metadata
+        alias :array? :array
 
-      def initialize(*, max_identifier_length: 63, **)
-        super
-        @max_identifier_length = max_identifier_length
-      end
+        def initialize(*, serial: nil, **)
+          super
+          @serial = serial
+        end
 
-      def serial?
-        return unless default_function
-
-        if %r{\Anextval\('"?(?<sequence_name>.+_(?<suffix>seq\d*))"?'::regclass\)\z} =~ default_function
-          sequence_name_from_parts(table_name, name, suffix) == sequence_name
+        def serial?
+          @serial
         end
       end
-
-      private
-        attr_reader :max_identifier_length
-
-        def sequence_name_from_parts(table_name, column_name, suffix)
-          over_length = [table_name, column_name, suffix].map(&:length).sum + 2 - max_identifier_length
-
-          if over_length > 0
-            column_name_length = [(max_identifier_length - suffix.length - 2) / 2, column_name.length].min
-            over_length -= column_name.length - column_name_length
-            column_name = column_name[0, column_name_length - [over_length, 0].min]
-          end
-
-          if over_length > 0
-            table_name = table_name[0, table_name.length - over_length]
-          end
-
-          "#{table_name}_#{column_name}_#{suffix}"
-        end
     end
+    PostgreSQLColumn = PostgreSQL::Column # :nodoc:
   end
 end

activerecord/lib/active_record/connection_adapters/postgresql/schema_statements.rb
@@ -650,16 +650,19 @@ module ActiveRecord
             default_value = extract_value_from_default(default)
             default_function = extract_default_function(default_value, default)
 
-            PostgreSQLColumn.new(
+            if match = default_function&.match(/\Anextval\('"?(?<sequence_name>.+_(?<suffix>seq\d*))"?'::regclass\)\z/)
+              serial = sequence_name_from_parts(table_name, column_name, match[:suffix]) == match[:sequence_name]
+            end
+
+            PostgreSQL::Column.new(
               column_name,
               default_value,
               type_metadata,
               !notnull,
-              table_name,
               default_function,
-              collation,
+              collation: collation,
               comment: comment.presence,
-              max_identifier_length: max_identifier_length
+              serial: serial
             )
           end
 
@@ -673,6 +676,22 @@ module ActiveRecord
               scale: cast_type.scale,
             )
             PostgreSQLTypeMetadata.new(simple_type, oid: oid, fmod: fmod)
+          end
+
+          def sequence_name_from_parts(table_name, column_name, suffix)
+            over_length = [table_name, column_name, suffix].sum(&:length) + 2 - max_identifier_length
+
+            if over_length > 0
+              column_name_length = [(max_identifier_length - suffix.length - 2) / 2, column_name.length].min
+              over_length -= column_name.length - column_name_length
+              column_name = column_name[0, column_name_length - [over_length, 0].min]
+            end
+
+            if over_length > 0
+              table_name = table_name[0, table_name.length - over_length]
+            end
+
+            "#{table_name}_#{column_name}_#{suffix}"
           end
 
           def extract_foreign_key_action(specifier)

activerecord/lib/active_record/connection_adapters/sqlite3/schema_statements.rb
@@ -105,7 +105,7 @@ module ActiveRecord
               end
 
             type_metadata = fetch_type_metadata(field["type"])
-            Column.new(field["name"], default, type_metadata, field["notnull"].to_i == 0, table_name, nil, field["collation"])
+            Column.new(field["name"], default, type_metadata, field["notnull"].to_i == 0, collation: field["collation"])
           end
 
           def data_source_sql(name = nil, type: nil)

ManageIQ/manageiq-schema

commit 96473729881dcc323d9ce121d27f5f1be99a960b

[rails6][schema_dumper.rb] Use table_name properly
A change that was introduced in Rails 6:

https://github.com/rails/rails/pull/35890/commits/f185e0ebc9cc18c596eb9cab1a9cd99e26a20500

Caused `column.table_name` to be an invalid call, and thus the schema
was not printed out properly.  This wasn't noticed originally because it
was silently passing thanks to this rescue statement:

https://github.com/rails/rails/blob/fe76a95b0d252a2d7c25e69498b720c96b243ea2/activerecord/lib/active_record/schema_dumper.rb#L165-L169

The error that ended up being displayed in the schema was something
like:

  ActiveRecord::Schema.define(version: 2021_01_06_200226) do

    # These are extensions that must be enabled in order to support this database
    enable_extension "plpgsql"

    # Could not dump table "accounts" because of following NoMethodError #   undefined method `table_name' for
    #<ActiveRecord::ConnectionAdapters::PostgreSQL::Column:0x000055ae1dbe7d70>

In the same Rails commit that removed the `column.table_name`, a
`table_name` attribute was added as an attr_accessor to `SchemaDumper`
to track that current `table_name` being worked on, so that was used in
this fix instead of the previous `table_name` method on `Column`.

* * *

This commit also adds schema_dumper_spec.rb, which is meant to catch
issues like this in the future in CI instead of by another user.

lib/manageiq/schema/schema_dumper.rb
@@ -119,7 +119,7 @@ module ManageIQ
 
       def column_spec_for_primary_key(column)
         result = super
-        result.delete(:default) if column.table_name.match?(METRIC_ROLLUP_TABLE_REGEXP)
+        result.delete(:default) if table_name.match?(METRIC_ROLLUP_TABLE_REGEXP)
         result
       end
     end

lib/tasks_private/spec.rake
@@ -1,5 +1,5 @@
 desc "Run all specs"
-task :spec => ["spec:non_migration", "spec:migrations"]
+task :spec => ["spec:non_migration", "spec:migrations", "spec:schema"]
 
 namespace :spec do
   task :initialize do
@@ -18,8 +18,30 @@ namespace :spec do
   desc "Prepare all specs"
   task :setup => [:initialize, :setup_db]
 
+  desc <<~DESC
+    Generate table list for schema specs
+
+    Generates a list of tables from the `spec/dummy/db/schema.rb` to be used in
+    the `schema_dumper_spec.rb`.
+  DESC
+  task :table_list => "spec:migrations:complete_up" do
+    File.open("spec/support/table_list.txt", "w") do |table_list|
+      File.open("spec/dummy/db/schema.rb").each_line do |line|
+        next unless line =~ /^\s*create_table\s+"([^"]+)"/
+
+        table_list.puts $1
+      end
+    end
+  end
+
   desc "Run all non-migration specs"
   RSpec::Core::RakeTask.new(:non_migration => :initialize) do |t|
     t.pattern = FileList["spec/**/*_spec.rb"].exclude("spec/migrations/**/*_spec.rb")
+                                             .exclude("spec/schema/**/*_spec.rb")
+  end
+
+  desc "Run schema specs"
+  RSpec::Core::RakeTask.new(:schema => [:initialize, "spec:migrations:complete_up"]) do |t|
+    t.pattern = FileList["spec/schema/**/*_spec.rb"]
   end
 end

spec/schema/schema_dumper_spec.rb
@@ -1,0 +1,77 @@
+describe "SchemaDumper" do
+  SCHEMA_RB_FILE  = File.expand_path("../dummy/db/schema.rb", __dir__)
+  TABLE_LIST_FILE = File.expand_path("../support/table_list.txt", __dir__)
+
+  let(:combined_metric_list) { metric_rollup_list + metric_list }
+  let(:metric_list)          { (0..23).to_a.map { |num| "metrics_#{num.to_s.rjust(2, '0')}" } }
+  let(:metric_rollup_list)   { (1..12).to_a.map { |num| "metric_rollups_#{num.to_s.rjust(2, '0')}" } }
+  let(:table_list)           { File.read(TABLE_LIST_FILE).lines.map(&:strip) }
+  let(:schema)               { File.read(SCHEMA_RB_FILE) }
+
+  let(:missing_table_failure_message) do
+    <<~MSG.lines.map(&:strip).join(" ").prepend("\n")
+      If this spec failed and a new migration was added that adds/removes a
+      table, please run `spec:table_list` to ensure that `table_list.txt` is up
+      to date with the newest schema.
+    MSG
+  end
+
+  it "includes all of the manageiq tables" do
+    create_table_lines = schema.lines.grep(/^\s*create_table/)
+
+    expect(create_table_lines.count).to eq(table_list.count), missing_table_failure_message
+
+    table_list.each do |table_name|
+      has_table = create_table_lines.detect { |l| l.include?(table_name) }
+      expect(has_table).to be_truthy, "Table '#{table_name}' not found in schema\n#{missing_table_failure_message}"
+    end
+  end
+
+  it "includes all id column comments" do
+    id_comment_list  = %w[conversion_hosts miq_tasks vms]
+    id_comment_lines = schema.lines.grep(/^\s*change_column_comment/)
+
+    expect(id_comment_lines.count).to eq(id_comment_list.count)
+
+    id_comment_list.each do |table_name|
+      has_table = id_comment_lines.detect { |l| l.include?(table_name) }
+      expect(has_table).to be_truthy, "comment not added for id in schema for '#{table_name}'"
+    end
+  end
+
+  it "includes all metrics sequences" do
+    miq_metric_sequences = schema.lines.grep(/^\s*change_miq_metric_sequence/)
+
+    expect(miq_metric_sequences.count).to eq(metric_list.count + metric_rollup_list.count)
+
+    combined_metric_list.each do |metric_subdivision|
+      has_definition = miq_metric_sequences.detect { |l| l.include?(metric_subdivision) }
+      expect(has_definition).to be_truthy, "change_miq_metric_sequence not defined for #{metric_subdivision}"
+    end
+  end
+
+  it "includes all metrics table inheritence" do
+    miq_metric_inheritances = schema.lines.grep(/^\s*add_miq_metric_table_inheritance/)
+
+    expect(miq_metric_inheritances.count).to eq(metric_list.count + metric_rollup_list.count)
+
+    combined_metric_list.each do |metric_subdivision|
+      has_definition = miq_metric_inheritances.detect { |l| l.include?(metric_subdivision) }
+      expect(has_definition).to be_truthy, "add_miq_metric_table_inheritance not defined for #{metric_subdivision}"
+    end
+  end
+
+  it "includes all views" do
+    relevant_schema_lines = schema.lines.grep(/create_miq_metric_view/).join
+
+    expect(relevant_schema_lines).to include 'create_miq_metric_view "metrics"'
+    expect(relevant_schema_lines).to include 'create_miq_metric_view "metric_rollups"'
+  end
+
+  it "includes all triggers" do
+    relevant_schema_lines = schema.lines.grep(/add_trigger/).join
+
+    expect(relevant_schema_lines).to include 'add_trigger "insteadof", "metrics", "metrics_partition",'
+    expect(relevant_schema_lines).to include 'add_trigger "insteadof", "metric_rollups", "metric_rollups_partition",'
+  end
+end