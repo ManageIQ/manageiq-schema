5855c9fc0345b28cee4b934b066ad0c44983801a
Adam Grare <adam@grare.com>
2021-07-30 08:24:58 -0400

    Refactor settings change rename into a method

--- db/migrate/20210727140323_rename_job_proxy_dispatcher_settings.rb
+++ db/migrate/20210727140323_rename_job_proxy_dispatcher_settings.rb
@@ -4,35 +4,33 @@ class RenameJobProxyDispatcherSettings < ActiveRecord::Migration[6.0]
   end
 
   def up
-    schedule_worker_base_key = "/workers/worker_base/schedule_worker"
-
     %w[
       job_proxy_dispatcher_interval
       job_proxy_dispatcher_stale_message_timeout
       job_proxy_dispatcher_stale_message_check_interval
     ].each do |old_name|
-      old_key = "#{schedule_worker_base_key}/#{old_name}"
-      new_key = "#{schedule_worker_base_key}/#{old_name.gsub(/^job_proxy/, "vm_scan")}"
-
-      say_with_time("Renaming SettingsChange #{old_key}") do
-        SettingsChange.in_my_region.where(:key => old_key).update_all(:key => new_key)
-      end
+      rename_schedule_worker_settings_change(old_name, old_name.gsub(/^job_proxy/, "vm_scan"))
     end
   end
 
   def down
-    schedule_worker_base_key = "/workers/worker_base/schedule_worker"
-
     %w[
       vm_scan_dispatcher_interval
       vm_scan_dispatcher_stale_message_timeout
       vm_scan_dispatcher_stale_message_check_interval
     ].each do |old_name|
-      old_key = "#{schedule_worker_base_key}/#{old_name}"
-      new_key = "#{schedule_worker_base_key}/#{old_name.gsub(/^vm_scan/, "job_proxy")}"
-      say_with_time("Renaming SettingsChange #{old_key}") do
-        SettingsChange.in_my_region.where(:key => old_key).update_all(:key => new_key)
-      end
+      rename_schedule_worker_settings_change(old_name, old_name.gsub(/^vm_scan/, "job_proxy"))
+    end
+  end
+
+  private
+
+  def rename_schedule_worker_settings_change(old_name, new_name, base_path = "/workers/worker_base/schedule_worker")
+    old_key = "#{base_path}/#{old_name}"
+    new_key = "#{base_path}/#{new_name}"
+
+    say_with_time("Renaming SettingsChange #{old_key}") do
+      SettingsChange.in_my_region.where(:key => old_key).update_all(:key => new_key)
     end
   end
 end
--- spec/migrations/20210727140323_rename_job_proxy_dispatcher_settings_spec.rb
+++ spec/migrations/20210727140323_rename_job_proxy_dispatcher_settings_spec.rb
@@ -5,13 +5,13 @@ describe RenameJobProxyDispatcherSettings do
 
   migration_context :up do
     it "removes any job_proxy_dispatcher_interval settings changes" do
-      settings_change_1 = settings_change_stub.create!(:key => "/workers/worker_base/schedule_worker/job_proxy_dispatcher_interval", :value => "30.seconds")
-      settings_change_2 = settings_change_stub.create!(:key => "/workers/worker_base/schedule_worker/job_proxy_dispatcher_stale_message_timeout", :value => "30.seconds")
-      settings_change_3 = settings_change_stub.create!(:key => "/workers/worker_base/schedule_worker/job_proxy_dispatcher_stale_message_check_interval", :value => "30.seconds")
+      settings_change1 = settings_change_stub.create!(:key => "/workers/worker_base/schedule_worker/job_proxy_dispatcher_interval", :value => "30.seconds")
+      settings_change2 = settings_change_stub.create!(:key => "/workers/worker_base/schedule_worker/job_proxy_dispatcher_stale_message_timeout", :value => "30.seconds")
+      settings_change3 = settings_change_stub.create!(:key => "/workers/worker_base/schedule_worker/job_proxy_dispatcher_stale_message_check_interval", :value => "30.seconds")
       migrate
-      expect(settings_change_1.reload.key).to eq("/workers/worker_base/schedule_worker/vm_scan_dispatcher_interval")
-      expect(settings_change_2.reload.key).to eq("/workers/worker_base/schedule_worker/vm_scan_dispatcher_stale_message_timeout")
-      expect(settings_change_3.reload.key).to eq("/workers/worker_base/schedule_worker/vm_scan_dispatcher_stale_message_check_interval")
+      expect(settings_change1.reload.key).to eq("/workers/worker_base/schedule_worker/vm_scan_dispatcher_interval")
+      expect(settings_change2.reload.key).to eq("/workers/worker_base/schedule_worker/vm_scan_dispatcher_stale_message_timeout")
+      expect(settings_change3.reload.key).to eq("/workers/worker_base/schedule_worker/vm_scan_dispatcher_stale_message_check_interval")
     end
 
     it "doesn't remove unrelated settings changes" do
@@ -23,13 +23,13 @@ describe RenameJobProxyDispatcherSettings do
 
   migration_context :down do
     it "removes any job_proxy_dispatcher_interval settings changes" do
-      settings_change_1 = settings_change_stub.create!(:key => "/workers/worker_base/schedule_worker/vm_scan_dispatcher_interval", :value => "30.seconds")
-      settings_change_2 = settings_change_stub.create!(:key => "/workers/worker_base/schedule_worker/vm_scan_dispatcher_stale_message_timeout", :value => "30.seconds")
-      settings_change_3 = settings_change_stub.create!(:key => "/workers/worker_base/schedule_worker/vm_scan_dispatcher_stale_message_check_interval", :value => "30.seconds")
+      settings_change1 = settings_change_stub.create!(:key => "/workers/worker_base/schedule_worker/vm_scan_dispatcher_interval", :value => "30.seconds")
+      settings_change2 = settings_change_stub.create!(:key => "/workers/worker_base/schedule_worker/vm_scan_dispatcher_stale_message_timeout", :value => "30.seconds")
+      settings_change3 = settings_change_stub.create!(:key => "/workers/worker_base/schedule_worker/vm_scan_dispatcher_stale_message_check_interval", :value => "30.seconds")
       migrate
-      expect(settings_change_1.reload.key).to eq("/workers/worker_base/schedule_worker/job_proxy_dispatcher_interval")
-      expect(settings_change_2.reload.key).to eq("/workers/worker_base/schedule_worker/job_proxy_dispatcher_stale_message_timeout")
-      expect(settings_change_3.reload.key).to eq("/workers/worker_base/schedule_worker/job_proxy_dispatcher_stale_message_check_interval")
+      expect(settings_change1.reload.key).to eq("/workers/worker_base/schedule_worker/job_proxy_dispatcher_interval")
+      expect(settings_change2.reload.key).to eq("/workers/worker_base/schedule_worker/job_proxy_dispatcher_stale_message_timeout")
+      expect(settings_change3.reload.key).to eq("/workers/worker_base/schedule_worker/job_proxy_dispatcher_stale_message_check_interval")
     end
 
     it "doesn't remove unrelated settings changes" do
