c48ab0e531b04316bb32782b16210fc83557dc3a
Jason Frey <fryguy9@gmail.com>
2021-11-03 16:21:43 -0400

    Restore changes needed to run the schema plugin in the dummy app
    Edited version of c89dbcfd89bead6f2dc7cfc1a7ef3e32b62e1073
    
    Changes:
    
    Added 9ad9001485588cdd8742e4f54cb95c42c0499e34 (YAML coder default)
    Dropped 7e34b02c551d25892511e702029900fb049ec39b (dummy/Gemfile is removed)
    
    Contains:
    
    Commit b15f5f5766d6a429d63652c79a881b97a996bf8e:
    - Remove the .ruby-version restriction
    - Load the manageiq-schema Gemfile instead of the dummy app Gemfile
    - Delete the generated database.yml in favor of database.tmpl.yml
    - Ignore the db/schema.rb that is generated
    - Restore the v2_key
    
    Commit c716815961acde41c1d1dbd2a7c362504e23db62:
    - Temporary override of the belongs_to_required_by_default setting
    
    Commit 6b7d6fd2e129e4592c6d2545721a16c995f1fcd5:
    - Update the dummy app to use_yaml_unsafe_load
    
    Commit 8529571f1a45c254fd9f4267c127a52f87a6fd9a:
    - Drop config.assets since sprockets-rails isn't in rails 7
    
    Commit 9ad9001485588cdd8742e4f54cb95c42c0499e34
    - Set default_column_serializer to YAML for now for backward compatibility
      (Changed to drop the 'if rails 7.1' conditional)

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults Rails::VERSION::STRING.to_f
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -23,5 +23,16 @@ module Dummy
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # HACK: Temporary override of the default setting until we can update the
+    # migration specs to honor it.
+    config.active_record.belongs_to_required_by_default = false
+
+    # Note, you can't pass kwargs :coder => YAML to serialize until rails 7.1 as it was a positional
+    # argument previously.  To avoid a case statement in all usages of serialize, we're defaulting
+    # all serialized columns to YAML for rails 7.1+ here. Ideally, we would use JSON if we find we can
+    # use a simpler datatype. See: https://github.com/rails/rails/pull/47463
+    config.active_record.default_column_serializer = YAML
+    config.active_record.use_yaml_unsafe_load = true
   end
 end
