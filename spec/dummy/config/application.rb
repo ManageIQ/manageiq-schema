c716815961acde41c1d1dbd2a7c362504e23db62
Jason Frey <fryguy9@gmail.com>
2021-11-04 15:31:04 -0400

    Temporary override of the belongs_to_required_by_default setting
    Temporary override of the belongs_to_required_by_default settomgs until
    we can change the migrations specs to honor it

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -15,5 +15,9 @@ module Dummy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # HACK: Temporary override of the default setting until we can update the
+    # migration specs to honor it.
+    config.active_record.belongs_to_required_by_default = false
   end
 end
