10ff8963d7e8d0a2f171c14c5db8c1d028bbce1f
Joe Rafaniello <jrafanie@gmail.com>
2022-01-14 17:46:58 -0500

    Drop unused pglogical references
    As of https://github.com/ManageIQ/manageiq/pull/18686 we moved from pglogical
    to the pg-logical_replication using postgresql's native logical replication.
    
    We switched as of ivanchuk.  It should be safe to drop this now.
    
    Funny enough, this actually helps with getting ruby 3.0 support because the AR
    extension signature for drop_table needs to change to work with ruby 2.x and
    3.x.

--- db/migrate/20171031010000_add_schema_migrations_ran.rb
+++ db/migrate/20171031010000_add_schema_migrations_ran.rb
@@ -4,16 +4,6 @@ class AddSchemaMigrationsRan < ActiveRecord::Migration[5.0]
       t.string :version
       t.datetime :created_at, :null => false
     end
-
-    require 'pg/pglogical'
-    require 'pg/pglogical/active_record_extension'
-
-    pglogical = ActiveRecord::Base.connection.pglogical
-    if pglogical.enabled? && pglogical.replication_sets.include?('miq')
-      # don't do an initial data sync for this table
-      # we want the data do come in as it's replicated rather than in bulk
-      pglogical.replication_set_add_table('miq', "schema_migrations_ran", false)
-    end
   end
 
   def down
