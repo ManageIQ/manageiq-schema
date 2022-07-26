e433e9eaee547ef28fb3e8133ee327de8b8d2277
Akhil K R <akhilkr128@gmail.com>
2022-07-26 22:20:40 +0530

    Adding request id

--- db/migrate/20220512123911_create_request_logs.rb
+++ db/migrate/20220512123911_create_request_logs.rb
@@ -1,6 +1,7 @@
 class CreateRequestLogs < ActiveRecord::Migration[6.0]
   def change
     create_table :request_logs do |t|
+      t.references :miq_requests, :type => :bigint
       t.string :log_message
       t.string :object_type
       t.bigint :object_id
