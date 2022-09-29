0aedc2dd5a3a9f99312f07351ab2f0ca9693f0d5
Or Gur <Or.Gur@ocean.ibm.com>
2022-09-29 10:14:38 +0300

    capabilities schemas, capabilities work, values still do the strange bug with retrieving host cluster instead of capability value

--- db/migrate/20220920133757_create_storage_capabilities.rb
+++ db/migrate/20220920133757_create_storage_capabilities.rb
@@ -3,6 +3,7 @@ class CreateStorageCapabilities < ActiveRecord::Migration[6.1]
     create_table :storage_capabilities do |t|
       t.string :name
       t.string :ems_ref
+      t.references :ems, :type => :bigint, :index => true, :references => :ext_management_system
       t.timestamps
     end
   end
--- db/migrate/20220920133812_create_storage_capability_values.rb
+++ db/migrate/20220920133812_create_storage_capability_values.rb
@@ -3,8 +3,9 @@ class CreateStorageCapabilityValues < ActiveRecord::Migration[6.1]
     create_table :storage_capability_values do |t|
       t.string :name
       t.string :value
-      t.references :storage_capability, :type => :bigint, :index => true
       t.string :ems_ref
+      t.references :storage_capability, :type => :bigint, :index => true
+      t.references :ems, :type => :bigint, :index => true, :references => :ext_management_system
       t.timestamps
     end
   end
