f8a0f081e73399ff9c9a5e3dc8d681dd7a183115
Or Gur <Or.Gur@ocean.ibm.com>
2022-10-02 15:46:43 +0300

    storage capability and values, values still can't get ems_id properly and it seems to cause dup values in every refresh

--- db/migrate/20220920133812_create_storage_capability_values.rb
+++ db/migrate/20220920133812_create_storage_capability_values.rb
@@ -1,7 +1,6 @@
 class CreateStorageCapabilityValues < ActiveRecord::Migration[6.1]
   def change
     create_table :storage_capability_values do |t|
-      t.string :name
       t.string :value
       t.string :ems_ref
       t.references :storage_capability, :type => :bigint, :index => true
