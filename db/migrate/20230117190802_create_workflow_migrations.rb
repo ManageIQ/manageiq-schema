c1d2f5c6a91e31f23ee6a1e023772acd5dd668f0
Adam Grare <adam@grare.com>
2023-03-10 10:03:22 -0500

    Add evm_owner and miq_group

--- db/migrate/20230117190802_create_workflow_migrations.rb
+++ db/migrate/20230117190802_create_workflow_migrations.rb
@@ -4,6 +4,8 @@ class CreateWorkflowMigrations < ActiveRecord::Migration[6.1]
       t.references :ems,           :type => :bigint, :index => true, :references => :ext_management_system
       t.references :tenant,        :type => :bigint, :index => true
       t.references :git_reference, :type => :bigint, :index => true
+      t.references :evm_owner,     :type => :bigint, :index => true, :references => :user
+      t.references :miq_group,     :type => :bigint, :index => true
       t.string :type, :index => true
       t.string :name
       t.jsonb :workflow_content, :default => {}
@@ -16,6 +18,8 @@ class CreateWorkflowMigrations < ActiveRecord::Migration[6.1]
       t.references :ems,      :type => :bigint, :index => true, :references => :ext_management_system
       t.references :workflow, :type => :bigint, :index => true
       t.references :tenant,   :type => :bigint, :index => true
+      t.references :evm_owner,     :type => :bigint, :index => true, :references => :user
+      t.references :miq_group,     :type => :bigint, :index => true
       t.references :miq_task, :type => :bigint, :index => true
       t.string :type, :index => true
       t.string :userid
