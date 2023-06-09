56745d030679ec21b06c31fe9ca21e6f756547e3
Jason Frey <fryguy9@gmail.com>
2023-06-09 13:52:22 -0400

    Add spec to check whether foreign key columns are in an index

--- spec/support/database_helper.rb
+++ spec/support/database_helper.rb
@@ -1,6 +1,10 @@
 module DatabaseHelper
   def self.columns_for_table(table_name)
-    ActiveRecord::Base.connection.data_source_exists?(table_name) ? ActiveRecord::Base.connection.columns(table_name) : []
+    ActiveRecord::Base.connection.data_source_exists?(table_name) ? ActiveRecord::Base.connection.columns(table_name).sort_by(&:name) : []
+  end
+
+  def self.indexes_for_table(table_name)
+    ActiveRecord::Base.connection.data_source_exists?(table_name) ? ActiveRecord::Base.connection.indexes(table_name).sort_by(&:name) : []
   end
 
   def self.table_has_type_column?(table_name)
