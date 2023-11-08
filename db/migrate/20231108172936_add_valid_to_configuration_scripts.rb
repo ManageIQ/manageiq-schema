bf65b56086eb93e4986e2d38febd01897fd076eb
Adam Grare <adam@grare.com>
2023-11-08 14:19:35 -0500

    Default to payload_valid=t

--- db/migrate/20231108172936_add_valid_to_configuration_scripts.rb
+++ db/migrate/20231108172936_add_valid_to_configuration_scripts.rb
@@ -1,6 +1,6 @@
 class AddValidToConfigurationScripts < ActiveRecord::Migration[6.1]
   def change
-    add_column :configuration_scripts, :payload_valid, :boolean
+    add_column :configuration_scripts, :payload_valid, :boolean, :default => true
     add_column :configuration_scripts, :payload_error, :string
   end
 end
