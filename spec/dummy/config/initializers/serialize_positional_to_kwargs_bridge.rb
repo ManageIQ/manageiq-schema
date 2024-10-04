410fbfcdabbec411accaf0091e59f4abf3fd3164
Joe Rafaniello <jrafanie@gmail.com>
2024-10-04 16:11:59 -0400

    Use the rails 7.1+ serialize interface even for 7.0
    We can intercept serialize for 7.0 and convert kwargs back to
    the positional class_name_or_coder.
    
    Update existing migrations to use rails 7.1 interface.

--- spec/dummy/config/initializers/serialize_positional_to_kwargs_bridge.rb
+++ spec/dummy/config/initializers/serialize_positional_to_kwargs_bridge.rb
@@ -1,21 +1,17 @@
 module Dummy
   module SerializePositionalToKwargsBridge
     def serialize(*args, **options)
-      return super if Rails.version < "7.1" || options[:coder]
+      return super if Rails.version >= "7.1"
 
-      # If class_name_or_coder second argument is a class, set the type
-      if args[1].respond_to?(:new)
-        options = options.merge(coder: YAML, type: args[1])
-      # If no class or coder provided, assume YAML coder and Object (legacy defaults / no validation)
-      elsif args[1].blank?
-        options = options.merge(coder: YAML, type: Object)
-      else
-        # otherwise, it's a coder which should only define dump/load, so set coder
-        options = options.merge(coder: args[1], type: Object)
+      # For rails 7.0.x, convert 7.1+ kwargs for coder/type into the positional argument
+      # class_name_or_coder
+      if options[:coder]
+        args << options.delete(:coder)
+      elsif options[:type]
+        args << options.delete(:type)
       end
 
-      # pass the first argument (drop the second) and forward the updated kwargs
-      super(args[0], **options)
+      super(*args, **options)
     end
   end
 end
