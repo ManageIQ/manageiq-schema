37e87fa519bb9df9f8fe41d9c005b9b1790b4a32
Jason Frey <fryguy9@gmail.com>
2024-10-23 16:29:10 -0400

    Merge pull request #757 from bdunne/scramsha
    Add a migration to encrypt database password using scram-sha-256
    
    (cherry picked from commit 3762bf55c1f5037c7e5421ec426ecbb9dad12fdb)

---
+++ db/migrate/20241017013023_reencrypt_password_scramsha.rb
@@ -0,0 +1,13 @@
+class ReencryptPasswordScramsha < ActiveRecord::Migration[6.1]
+  def up
+    say_with_time('Reencrypting database user password with scram-sha-256') do
+      db_config = ActiveRecord::Base.connection_db_config.configuration_hash
+      username = db_config[:username]
+      password = connection.raw_connection.encrypt_password(db_config[:password], username, "scram-sha-256")
+
+      connection.execute <<-SQL
+        ALTER ROLE #{username} WITH PASSWORD '#{password}';
+      SQL
+    end
+  end
+end
