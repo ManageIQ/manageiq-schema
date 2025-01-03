0f2cd766271ee860ab0619b740bca937a230360d
Joe Rafaniello <jrafanie@gmail.com>
2025-01-03 15:18:10 -0500

    Extract migration_path method
    Changed the behavior as require_migration expected to be called from
    the spec, but this method could be called from the helper or elsewhere.
    Therefore, we detect the first caller location with the spec instead of assuming
    it's the first one.

--- spec/migrations/20241017013023_reencrypt_password_scramsha_spec.rb
+++ spec/migrations/20241017013023_reencrypt_password_scramsha_spec.rb
@@ -11,7 +11,7 @@ describe ReencryptPasswordScramsha do
 
       allow(ActiveRecord::Base.connection_db_config).to receive(:configuration_hash).and_wrap_original do |original_method, *args, &block|
         # set a value for any calls originating from the migration file, not from rails itself
-        original_method.call(*args, &block).dup.tap {|i| i[:password] ||= "abc" if caller_locations.any? {|loc| loc.path.include?("db/migrate/20241017013023_reencrypt_password_scramsha.rb")} }
+        original_method.call(*args, &block).dup.tap { |i| i[:password] ||= "abc" if caller_locations.any? {|loc| loc.path.include?(migration_path)} }
       end
       expect(ActiveRecord::Base.connection).to receive(:execute).with(a_string_matching(/ALTER ROLE #{username} WITH PASSWORD \'SCRAM-SHA-256.*\'\;/))
 
@@ -25,7 +25,7 @@ describe ReencryptPasswordScramsha do
 
       allow(ActiveRecord::Base.connection_db_config).to receive(:configuration_hash).and_wrap_original do |original_method, *args, &block|
         # set a value for any calls originating from the migration file, not from rails itself
-        original_method.call(*args, &block).dup.tap { |i| i.delete(:password) if caller_locations.any? {|loc| loc.path.include?("db/migrate/20241017013023_reencrypt_password_scramsha.rb")} }
+        original_method.call(*args, &block).dup.tap { |i| i.delete(:password) if caller_locations.any? {|loc| loc.path.include?(migration_path)} }
       end
       expect(ActiveRecord::Base.connection).not_to receive(:execute).with(a_string_matching(/ALTER ROLE.*\'\;/))
 
